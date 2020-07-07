38ddabdd7e5ac9f7b56afe19c78affc253420346
zhengbinbin <zhengbinbin@58.com>
2020-07-07 23:39:53 +0800

    update

---
+++ src/com/geekbing/easy/LeetCode1046.java
@@ -0,0 +1,17 @@
+package com.geekbing.easy;
+
+/**
+ * todo
+ *
+ * @author bing
+ */
+public class LeetCode1046 {
+    public int lastStoneWeight(int[] stones) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        LeetCode1046 leetCode1046 = new LeetCode1046();
+        System.out.println(leetCode1046.lastStoneWeight(new int[]{2, 7, 4, 1, 8, 1}));
+    }
+}
