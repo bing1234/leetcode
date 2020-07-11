52db7c5598f87959a85f1ee2ea0e824d4fce336a
zhengbinbin <zhengbinbin@58.com>
2020-07-11 08:46:54 +0800

    update

--- src/com/geekbing/easy/LeetCode520.java
+++ src/com/geekbing/easy/LeetCode520.java
@@ -7,7 +7,32 @@ package com.geekbing.easy;
  */
 public class LeetCode520 {
     public boolean detectCapitalUse(String word) {
-        return false;
+        if (word.length() < 2) {
+            return true;
+        }
+        // 第一个字母是否是大写
+        boolean firstUpper = 'A' <= word.charAt(0) && word.charAt(0) <= 'Z';
+        // 第二个字母是否是大写
+        boolean secondUpper = 'A' <= word.charAt(1) && word.charAt(1) <= 'Z';
+
+        if (firstUpper && secondUpper) {
+            // 必须全部大写
+            for (int i = 2; i < word.length(); i++) {
+                if (word.charAt(i) < 'A' || word.charAt(i) > 'Z') {
+                    return false;
+                }
+            }
+        } else if (secondUpper) {
+            return false;
+        } else {
+            // 后面全部必须小写
+            for (int i = 2; i < word.length(); i++) {
+                if ('A' <= word.charAt(i) && word.charAt(i) <= 'Z') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
