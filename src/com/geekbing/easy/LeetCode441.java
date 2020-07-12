6fff81da36f35a75110d5cd726d0192d305d5f80
zhengbinbin <zhengbinbin@58.com>
2020-07-12 22:49:23 +0800

    update

---
+++ src/com/geekbing/easy/LeetCode441.java
@@ -0,0 +1,27 @@
+package com.geekbing.easy;
+
+/**
+ * @author bing
+ */
+public class LeetCode441 {
+    public int arrangeCoins(int n) {
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+            if (sum == n) {
+                return i;
+            }
+            if (sum > n) {
+                return i - 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        LeetCode441 leetCode441 = new LeetCode441();
+        System.out.println(leetCode441.arrangeCoins(5));
+        System.out.println(leetCode441.arrangeCoins(8));
+        System.out.println(leetCode441.arrangeCoins(2147483647));
+    }
+}
