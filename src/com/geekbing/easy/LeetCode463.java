ffcf82340ea662a46e17a1e6d6fa0ba50c2ed51a
zhengbinbin <zhengbinbin@58.com>
2020-08-09 17:19:13 +0800

    update

--- src/com/geekbing/easy/LeetCode463.java
+++ src/com/geekbing/easy/LeetCode463.java
@@ -1,13 +1,25 @@
 package com.geekbing.easy;
 
 /**
- * todo
- *
  * @author bing
  */
 public class LeetCode463 {
     public int islandPerimeter(int[][] grid) {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    result = result + 4;
+                    if (i + 1 < grid.length && grid[i + 1][j] == 1) {
+                        result = result - 2;
+                    }
+                    if (j + 1 < grid[i].length && grid[i][j + 1] == 1) {
+                        result = result - 2;
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
@@ -15,9 +27,13 @@ public class LeetCode463 {
 
         int[][] case1 = new int[4][4];
         case1[0] = new int[]{0, 1, 0, 0};
-        case1[0] = new int[]{1, 1, 1, 0};
-        case1[0] = new int[]{0, 1, 0, 0};
-        case1[0] = new int[]{1, 1, 0, 0};
+        case1[1] = new int[]{1, 1, 1, 0};
+        case1[2] = new int[]{0, 1, 0, 0};
+        case1[3] = new int[]{1, 1, 0, 0};
         System.out.println(leetCode463.islandPerimeter(case1));
+
+        int[][] case2 = new int[1][2];
+        case2[0] = new int[]{1, 1};
+        System.out.println(leetCode463.islandPerimeter(case2));
     }
 }
