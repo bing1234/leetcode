51aa4142225e72e87438b32aaaf65d777b9a6d18
zhengbinbin <zhengbinbin@58.com>
2020-08-09 11:36:03 +0800

    update

--- src/com/geekbing/easy/LeetCode234.java
+++ src/com/geekbing/easy/LeetCode234.java
@@ -12,27 +12,69 @@ public class LeetCode234 {
      * 4. 恢复链表
      */
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head, fast = head;
+        // 特殊情况处理
+        if (head == null || head.next == null) {
+            return true;
+        }
+        if (head.next.next == null) {
+            return head.val == head.next.val;
+        }
 
-        while (fast != null && fast.next != null && fast.next.next != null) {
+        // 快慢指针
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        // 最后需要删除的元素
-        ListNode needDelete;
-        if (fast != null && fast.next != null) {
+        // 为了保持一致，当节点数为偶数时候，添加一个最后需要删除的元素
+        ListNode needDelete = null;
+        if (fast.next != null) {
             needDelete = new ListNode(0);
             needDelete.next = slow.next;
             slow.next = needDelete;
             slow = slow.next;
             fast = fast.next;
         }
-        // 现在slow指向中间节点、fast指向最后一个节点
+        // 经过上面的处理后，现在slow指向中间节点、fast指向最后一个节点
+        // 从slow开始翻转链表
+        reverseList(slow);
 
+        // 判断是否是回文链表
+        ListNode index1 = head, index2 = fast;
+        while (index1 != null && index2 != null) {
+            if (index1.val != index2.val) {
+                return false;
+            }
+            index1 = index1.next;
+            index2 = index2.next;
+        }
 
+        // 结果
+        boolean result = index1 == null && index2 == null;
 
+        // 恢复链表
+        reverseList(fast);
+        // 删除多余的节点
+        if (needDelete != null) {
+            needDelete.val = needDelete.next.val;
+            needDelete.next = needDelete.next.next;
+        }
+        return result;
+    }
 
-        return true;
+    private ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null, current = head, next = current.next;
+        while (next != null) {
+            current.next = pre;
+            pre = current;
+            current = next;
+            next = next.next;
+        }
+        current.next = pre;
+        return current;
     }
 
     public static void main(String[] args) {
@@ -41,12 +83,30 @@ public class LeetCode234 {
         ListNode head1 = new ListNode(1);
         head1.next = new ListNode(2);
         System.out.println(leetCode234.isPalindrome(head1));
+        print(head1);
 
         ListNode head2 = new ListNode(1);
         head2.next = new ListNode(2);
         head2.next.next = new ListNode(2);
         head2.next.next.next = new ListNode(1);
         System.out.println(leetCode234.isPalindrome(head2));
+        print(head2);
+
+        ListNode head3 = new ListNode(1);
+        head3.next = new ListNode(2);
+        head3.next.next = new ListNode(2);
+        head3.next.next.next = new ListNode(2);
+        head3.next.next.next.next = new ListNode(1);
+        System.out.println(leetCode234.isPalindrome(head3));
+        print(head3);
+    }
+
+    private static void print(ListNode head) {
+        ListNode index = head;
+        while (index != null) {
+            System.out.println(index.val);
+            index = index.next;
+        }
     }
 
     private static class ListNode {
