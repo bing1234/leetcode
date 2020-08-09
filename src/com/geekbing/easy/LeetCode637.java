e92b5bac44b0eb5c3c84d1553c654382992f3b43
zhengbinbin <zhengbinbin@58.com>
2020-08-09 17:31:09 +0800

    update

--- src/com/geekbing/easy/LeetCode637.java
+++ src/com/geekbing/easy/LeetCode637.java
@@ -1,17 +1,41 @@
 package com.geekbing.easy;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
- * todo
- *
  * @author bing
  */
 public class LeetCode637 {
     public List<Double> averageOfLevels(TreeNode root) {
-
-        return new ArrayList<>();
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            double levelSum = 0D;
+            // 队列的大小就是当前层的大小
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                if (node == null) {
+                    continue;
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                levelSum = levelSum + node.val;
+            }
+            result.add(levelSum / levelSize);
+        }
+        return result;
     }
 
     public static void main(String[] args) {
