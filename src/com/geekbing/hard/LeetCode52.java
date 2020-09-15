2f2ca3a2093ed6854f804bbeba8d0602f1b7fdfd
bing <dhuzbb@163.com>
2020-09-15 23:45:19 +0800

    每日一题

---
+++ src/com/geekbing/hard/LeetCode52.java
@@ -0,0 +1,12 @@
+package com.geekbing.hard;
+
+public class LeetCode52 {
+    public int totalNQueens(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        LeetCode52 leetCode52 = new LeetCode52();
+        System.out.println(leetCode52.totalNQueens(4));
+    }
+}
