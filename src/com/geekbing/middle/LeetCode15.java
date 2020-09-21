e6470621f30457319485acef242adbdc11e7e69c
bing <dhuzbb@163.com>
2020-09-21 22:40:40 +0800

    每日一题

---
+++ src/com/geekbing/middle/LeetCode15.java
@@ -0,0 +1,15 @@
+package com.geekbing.middle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        LeetCode15 leetCode15 = new LeetCode15();
+        System.out.println(leetCode15.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+}
