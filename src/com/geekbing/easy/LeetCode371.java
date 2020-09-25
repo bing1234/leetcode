5a11dfb648c46de062f5813b703463489dc567da
bing <dhuzbb@163.com>
2020-09-25 23:56:36 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode371.java
+++ src/com/geekbing/easy/LeetCode371.java
@@ -1,18 +1,30 @@
 package com.geekbing.easy;
 
 /**
- * todo
- *
  * @author bing
  */
 public class LeetCode371 {
     public int getSum(int a, int b) {
-        return 0;
+        if (a == 0) {
+            return b;
+        }
+        if (b == 0) {
+            return a;
+        }
+        // 低位
+        int lower = a ^ b;
+        // 进位
+        int carry = a & b;
+        if (carry == 0) {
+            return lower;
+        }
+        return getSum(lower, carry << 1);
     }
 
     public static void main(String[] args) {
         LeetCode371 leetCode371 = new LeetCode371();
         System.out.println(leetCode371.getSum(1, 2));
         System.out.println(leetCode371.getSum(-2, 3));
+        System.out.println(leetCode371.getSum(-1, 1));
     }
 }
