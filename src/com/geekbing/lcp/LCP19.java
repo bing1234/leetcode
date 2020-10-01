a2aa66bb1feebbc4095a934190cb24ecb24170dd
bing <dhuzbb@163.com>
2020-10-02 00:05:04 +0800

    每日一题

--- src/com/geekbing/lcp/LCP19.java
+++ src/com/geekbing/lcp/LCP19.java
@@ -12,21 +12,13 @@ public class LCP19 {
         dp[0][0] = leaves.charAt(0) == 'y' ? 1 : 0;
         dp[0][1] = dp[0][2] = dp[1][2] = Integer.MAX_VALUE;
         for (int i = 1; i < leaves.length(); i++) {
-            dp[i][0] = dp[i - 1][0] + isYellow(leaves.charAt(i));
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][1]) + isRed(leaves.charAt(i));
+            dp[i][0] = dp[i - 1][0] + (leaves.charAt(i) == 'y' ? 1 : 0);
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][1]) + (leaves.charAt(i) == 'r' ? 1 : 0);
             if (i >= 2) {
-                dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][2]) + isYellow(leaves.charAt(i));
+                dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][2]) + (leaves.charAt(i) == 'y' ? 1 : 0);
             }
         }
         return dp[leaves.length() - 1][2];
-    }
-
-    private int isYellow(char c) {
-        return c == 'y' ? 1 : 0;
-    }
-
-    private int isRed(char c) {
-        return c == 'r' ? 1 : 0;
     }
 
     public static void main(String[] args) {
