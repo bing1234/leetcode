2a9495dc49d3e0707ccc4d7d75a459f2d7a0b221
bing <dhuzbb@163.com>
2020-10-02 11:44:20 +0800

    每日一题

---
+++ src/com/geekbing/middle/LeetCode535.java
@@ -0,0 +1,17 @@
+package com.geekbing.middle;
+
+public class LeetCode535 {
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        return "";
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
