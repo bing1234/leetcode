aec377ce4c050c343c999bad0d4bc186296d5b02
bing <dhuzbb@163.com>
2020-10-03 23:13:46 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode447.java
+++ src/com/geekbing/easy/LeetCode447.java
@@ -5,7 +5,6 @@ package com.geekbing.easy;
  */
 public class LeetCode447 {
     public int numberOfBoomerangs(int[][] points) {
-        return 0;
     }
 
     public static void main(String[] args) {
---
+++ src/com/geekbing/easy/LeetCode821.java
@@ -0,0 +1,13 @@
+package com.geekbing.easy;
+
+import java.util.Arrays;
+
+public class LeetCode821 {
+    public int[] shortestToChar(String S, char C) {
+    }
+
+    public static void main(String[] args) {
+        LeetCode821 leetCode821 = new LeetCode821();
+        System.out.println(Arrays.toString(leetCode821.shortestToChar("loveleetcode", 'e')));
+    }
+}
