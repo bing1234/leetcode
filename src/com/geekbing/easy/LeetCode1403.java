5f50bde69a607c8cbec15e49c89fd8bd64fe3968
bing <dhuzbb@163.com>
2020-10-03 23:04:02 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode1403.java
+++ src/com/geekbing/easy/LeetCode1403.java
@@ -1,11 +1,26 @@
 package com.geekbing.easy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LeetCode1403 {
     public List<Integer> minSubsequence(int[] nums) {
-        return new ArrayList<>();
+        Arrays.sort(nums);
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        int sum = 0;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans.add(nums[i]);
+            sum = sum + nums[i];
+            if (total - sum < sum) {
+                break;
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
