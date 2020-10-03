4036bcc4d81873e3bab47664f7269ff1ba41d23b
bing <dhuzbb@163.com>
2020-10-03 23:09:19 +0800

    每日一题

---
+++ src/com/geekbing/easy/LeetCode447.java
@@ -0,0 +1,15 @@
+package com.geekbing.easy;
+
+/**
+ * todo
+ */
+public class LeetCode447 {
+    public int numberOfBoomerangs(int[][] points) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        LeetCode447 leetCode447 = new LeetCode447();
+        System.out.println(leetCode447.numberOfBoomerangs(new int[][]{{0, 0}, {1, 0}, {2, 0}}));
+    }
+}
