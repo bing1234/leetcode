592771d06f83644fe819135f59b5c1a9b8a4f2d6
bing <dhuzbb@163.com>
2020-10-10 22:53:01 +0800

    每日一题

---
+++ src/com/geekbing/easy/LeetCode172.java
@@ -0,0 +1,16 @@
+package com.geekbing.easy;
+
+/**
+ * todo
+ */
+public class LeetCode172 {
+    public int trailingZeroes(int n) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        LeetCode172 leetCode172 = new LeetCode172();
+        System.out.println(leetCode172.trailingZeroes(3));
+        System.out.println(leetCode172.trailingZeroes(5));
+    }
+}
