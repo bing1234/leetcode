ee44887956e5f5bc0607a6af9c31850642070269
bing <dhuzbb@163.com>
2020-10-18 20:53:13 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode11.java
+++ src/com/geekbing/middle/LeetCode11.java
@@ -1,11 +1,33 @@
 package com.geekbing.middle;
 
 /**
- * todo
+ * https://leetcode-cn.com/problems/container-with-most-water/
+ * 11. 盛最多水的容器
+ *
+ * @author bing
  */
 public class LeetCode11 {
     public int maxArea(int[] height) {
-        return 0;
+        int left = 0, right = height.length - 1;
+        int ans = (right - left) * Math.min(height[left], height[right]);
+        while (left < right) {
+            if (height[left] < height[right]) {
+                // 移动左边的
+                int leftHeight = height[left];
+                while (left < right && height[left] <= leftHeight) {
+                    left++;
+                }
+                ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
+            } else {
+                // 移动右边的
+                int rightHeight = height[right];
+                while (left < right && height[right] <= rightHeight) {
+                    right--;
+                }
+                ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
