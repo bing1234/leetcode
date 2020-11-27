f7309f3ea69ed6e03254db43d896c98a552c636c
zhengbinbin <zhengbinbin@58.com>
2020-11-27 22:01:46 +0800

    每日一题

---
+++ src/com/geekbing/hard/LeetCode224.java
@@ -0,0 +1,124 @@
+package com.geekbing.hard;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class LeetCode224 {
+    public int calculate(String s) {
+        // 生成中缀表达式
+        List<String> infixExps = genInfixExps(s);
+        // 中缀表达式转后缀表达式
+        List<String> postfixExps = convertInfixToPostfix(infixExps);
+        // 计算后缀表达式
+        return calPostfixExps(postfixExps);
+    }
+
+    public static void main(String[] args) {
+        LeetCode224 leetCode224 = new LeetCode224();
+        System.out.println(leetCode224.calculate("1 + 1"));
+        System.out.println(leetCode224.calculate(" 2-1 + 2 "));
+        System.out.println(leetCode224.calculate("(1+(4+5+2)-3)+(6+8)"));
+    }
+
+    private List<String> genInfixExps(String str) {
+        List<String> list = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            char c = str.charAt(i);
+            if (c == ' ') {
+                i++;
+            } else if (Character.isDigit(c)) {
+                int num = 0;
+                while (i < str.length() && Character.isDigit(str.charAt(i))) {
+                    num = num * 10 + (str.charAt(i) - '0');
+                    i++;
+                }
+                list.add(String.valueOf(num));
+            } else {
+                list.add(String.valueOf(c));
+                i++;
+            }
+        }
+        return list;
+    }
+
+    private List<String> convertInfixToPostfix(List<String> infixExps) {
+        List<String> ans = new ArrayList<>();
+        Stack<Character> stack = new Stack<>();
+        stack.push('#');
+        for (String str : infixExps) {
+            if (Character.isDigit(str.charAt(0))) {
+                ans.add(str);
+            } else if (str.equals("(")) {
+                stack.push('(');
+            } else if (str.equals(")")) {
+                while (stack.peek() != '(') {
+                    ans.add(String.valueOf(stack.pop()));
+                }
+                stack.pop();
+            } else {
+                Character cur = str.charAt(0);
+                Character stackTop = stack.peek();
+                // 比较当前操作符和栈顶操作符的优先级
+                int temp = comparePriority(cur, stackTop);
+                if (temp <= 0) {
+                    ans.add(String.valueOf(stack.pop()));
+                }
+                stack.push(cur);
+            }
+        }
+        while (!stack.isEmpty() && stack.peek() != '#') {
+            ans.add(String.valueOf(stack.pop()));
+        }
+        return ans;
+    }
+
+    private int comparePriority(Character op1, Character op2) {
+        Integer p1 = getPriority(op1);
+        Integer p2 = getPriority(op2);
+        return p1.compareTo(p2);
+    }
+
+    private Integer getPriority(Character op) {
+        switch (op) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return 0;
+        }
+    }
+
+    private int calPostfixExps(List<String> list) {
+        Stack<Integer> stack = new Stack<>();
+        for (String str : list) {
+            if (Character.isDigit(str.charAt(0))) {
+                stack.push(Integer.parseInt(str));
+            } else {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                switch (str) {
+                    case "+":
+                        stack.push(num1 + num2);
+                        break;
+                    case "-":
+                        stack.push(num1 - num2);
+                        break;
+                    case "*":
+                        stack.push(num1 * num2);
+                        break;
+                    case "/":
+                        stack.push(num1 / num2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+}
