8472e4a89d8fbd4b316c50e30c4e4b0f34410fe4
zhengbinbin <zhengbinbin@58.com>
2020-12-04 22:31:50 +0800

    每日一题

---
+++ src/com/geekbing/middle/LeetCode659.java
@@ -0,0 +1,17 @@
+package com.geekbing.middle;
+
+/**
+ * todo 未做出
+ */
+public class LeetCode659 {
+    public boolean isPossible(int[] nums) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LeetCode659 leetCode659 = new LeetCode659();
+        System.out.println(leetCode659.isPossible(new int[]{1, 2, 3, 3, 4, 5}));
+        System.out.println(leetCode659.isPossible(new int[]{1, 2, 3, 3, 4, 4, 5, 5}));
+        System.out.println(leetCode659.isPossible(new int[]{1, 2, 3, 4, 4, 5}));
+    }
+}
