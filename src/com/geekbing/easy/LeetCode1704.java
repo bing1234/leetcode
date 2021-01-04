3e495bd661130af0c4068c4738e5f67d8ab5b50b
zhengbinbin <zhengbinbin@58.com>
2021-01-04 21:41:56 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode1704.java
+++ src/com/geekbing/easy/LeetCode1704.java
@@ -1,11 +1,37 @@
 package com.geekbing.easy;
 
-/**
- * todo
- */
+import java.util.HashSet;
+import java.util.Set;
+
 public class LeetCode1704 {
+    private static final Set<Character> set = new HashSet<>();
+
+    static {
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        set.add('A');
+        set.add('E');
+        set.add('I');
+        set.add('O');
+        set.add('U');
+    }
+
     public boolean halvesAreAlike(String s) {
-        return false;
+        int left = 0, leftCnt = 0, right = s.length() - 1, rightCnt = 0;
+        while (left < right) {
+            if (set.contains(s.charAt(left))) {
+                leftCnt++;
+            }
+            if (set.contains(s.charAt(right))) {
+                rightCnt++;
+            }
+            left++;
+            right--;
+        }
+        return leftCnt == rightCnt;
     }
 
     public static void main(String[] args) {
