bacea2599d08cd46aac520abccd4007cfc6c64b5
zhengbinbin <zhengbinbin@58.com>
2021-03-29 22:51:06 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode1624.java
+++ src/com/geekbing/easy/LeetCode1624.java
@@ -1,9 +1,54 @@
 package com.geekbing.easy;
 
-// todo
+/**
+ * @author bing
+ */
 public class LeetCode1624 {
     public int maxLengthBetweenEqualCharacters(String s) {
-        return 0;
+        Tuple[] buckets = new Tuple[26];
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            Tuple tuple = buckets[idx];
+            if (tuple == null) {
+                tuple = new Tuple(i, null);
+            } else {
+                tuple.setEnd(i);
+            }
+            buckets[idx] = tuple;
+        }
+        int ans = -1;
+        for (Tuple tuple : buckets) {
+            if (tuple != null && tuple.getBegin() != null && tuple.getEnd() != null) {
+                ans = Math.max(ans, tuple.getEnd() - tuple.getBegin() - 1);
+            }
+        }
+        return ans;
+    }
+
+    private static final class Tuple {
+        private Integer begin;
+        private Integer end;
+
+        public Tuple(Integer begin, Integer end) {
+            this.begin = begin;
+            this.end = end;
+        }
+
+        public Integer getBegin() {
+            return begin;
+        }
+
+        public void setBegin(Integer begin) {
+            this.begin = begin;
+        }
+
+        public Integer getEnd() {
+            return end;
+        }
+
+        public void setEnd(Integer end) {
+            this.end = end;
+        }
     }
 
     public static void main(String[] args) {
