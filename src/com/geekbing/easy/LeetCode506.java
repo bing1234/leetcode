ce75413569e42f7eb9fe520bd2991b8c2a7b432f
zhengbinbin <zhengbinbin@58.com>
2021-04-05 21:37:29 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode506.java
+++ src/com/geekbing/easy/LeetCode506.java
@@ -1,13 +1,34 @@
 package com.geekbing.easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * todo
+ * @author bing
  */
 public class LeetCode506 {
-    public String[] findRelativeRanks(int[] nums) {
-        return new String[]{};
+    public String[] findRelativeRanks(int[] score) {
+        int[] arr = Arrays.copyOf(score, score.length);
+        Arrays.sort(arr);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], i);
+        }
+        String[] ans = new String[score.length];
+        for (int i = 0; i < score.length; i++) {
+            Integer sortIdx = map.get(score[i]);
+            if (sortIdx == score.length - 1) {
+                ans[i] = "Gold Medal";
+            } else if (sortIdx == score.length - 2) {
+                ans[i] = "Silver Medal";
+            } else if (sortIdx == score.length - 3) {
+                ans[i] = "Bronze Medal";
+            } else {
+                ans[i] = String.valueOf(score.length - sortIdx);
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
