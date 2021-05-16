fb79b4118e043581d8a5d1965f92338c39f02749
zhengbinbin <zhengbinbin@58.com>
2021-05-16 21:08:25 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode5759.java
+++ src/com/geekbing/easy/LeetCode5759.java
@@ -1,11 +1,47 @@
 package com.geekbing.easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author bing
  */
 public class LeetCode5759 {
+    List<List<Integer>> ans;
+
     public int subsetXORSum(int[] nums) {
-        return 0;
+        // 先求出所有子集
+        ans = subsets(nums);
+
+        // 再求所有子集的异或和
+        int sum = 0;
+        for (List<Integer> subSet : ans) {
+            sum += xorSum(subSet);
+        }
+        return sum;
+    }
+
+    private int xorSum(List<Integer> set) {
+        int ans = 0;
+        for (int num : set) {
+            ans = ans ^ num;
+        }
+        return ans;
+    }
+
+    private List<List<Integer>> subsets(int[] nums) {
+        ans = new ArrayList<>();
+        backtrack(new ArrayList<>(), nums, 0);
+        return ans;
+    }
+
+    private void backtrack(List<Integer> cur, int[] nums, int i) {
+        ans.add(new ArrayList<>(cur));
+        for (int j = i; j < nums.length; j++) {
+            cur.add(nums[j]);
+            backtrack(cur, nums, j + 1);
+            cur.remove(cur.size() - 1);
+        }
     }
 
     public static void main(String[] args) {
