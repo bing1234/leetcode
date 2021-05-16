b7e0215aed574786c2a1b37a4f187feb44305887
zhengbinbin <zhengbinbin@58.com>
2021-05-16 20:45:44 +0800

    每日一题

---
+++ src/com/geekbing/lcp/LCP11.java
@@ -0,0 +1,27 @@
+package com.geekbing.lcp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author bing
+ */
+public class LCP11 {
+    public int expectNumber(int[] scores) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int score : scores) {
+            Integer count = countMap.get(score);
+            if (count == null) {
+                count = 0;
+            }
+            countMap.put(score, count + 1);
+        }
+        return countMap.keySet().size();
+    }
+
+    public static void main(String[] args) {
+        LCP11 lcp11 = new LCP11();
+        System.out.println(lcp11.expectNumber(new int[]{1, 2, 3}));
+        System.out.println(lcp11.expectNumber(new int[]{1, 1}));
+    }
+}
