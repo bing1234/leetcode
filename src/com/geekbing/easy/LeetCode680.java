3d038d1dd82a3a8007a567f339942353a5c72543
zhengbinbin <zhengbinbin@58.com>
2021-05-16 21:15:25 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode680.java
+++ src/com/geekbing/easy/LeetCode680.java
@@ -5,12 +5,16 @@ package com.geekbing.easy;
  */
 public class LeetCode680 {
     public boolean validPalindrome(String s) {
+        char[] chars = s.toCharArray();
 
-        return false;
+        return true;
     }
 
     public static void main(String[] args) {
         LeetCode680 leetCode680 = new LeetCode680();
+        System.out.println(leetCode680.validPalindrome("a"));
+        System.out.println(leetCode680.validPalindrome("ab"));
+        System.out.println(leetCode680.validPalindrome("abc"));
         System.out.println(leetCode680.validPalindrome("aba"));
         System.out.println(leetCode680.validPalindrome("abca"));
     }
