ce73bf808f807b350e860d8685360d54a58c184c
zhengbinbin <zhengbinbin@58.com>
2021-05-23 19:23:32 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode190.java
+++ src/com/geekbing/easy/LeetCode190.java
@@ -5,7 +5,55 @@ package com.geekbing.easy;
  */
 public class LeetCode190 {
     public int reverseBits(int n) {
-        return 0;
+        // 转换成二进制形式
+        int[] bits = binaryBits(n);
+        // 二进制反转
+        revertBits(bits);
+        // 计算转换成整数值
+        return calBinaryBits(bits);
+    }
+
+    private int[] binaryBits(int n) {
+        int[] bits = new int[32];
+        int idx = 31;
+        while (n != 0) {
+            bits[idx] = n & 1;
+            idx--;
+            n = n >>> 1;
+        }
+        return bits;
+    }
+
+    private void revertBits(int[] bits) {
+        int left = 0, right = bits.length - 1, temp;
+        while (left < right) {
+            // 交换
+            temp = bits[left];
+            bits[left] = bits[right];
+            bits[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    private int calBinaryBits(int[] bits) {
+        int ans = 0, base = 1;
+        if (bits[0] == 1) {
+            // 负数：1、先对各位取反；2、将其转换为十进制数；3、加上负号，再减去1
+            for (int i = 31; i >= 0; i--) {
+                ans += base * (bits[i] ^ 1);
+                base *= 2;
+            }
+            return -ans - 1;
+        } else {
+            // 正数
+            for (int i = 31; i >= 0; i--) {
+                ans += base * bits[i];
+                base *= 2;
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
--- src/com/geekbing/easy/LeetCode720.java
+++ src/com/geekbing/easy/LeetCode720.java
@@ -1,5 +1,7 @@
 package com.geekbing.easy;
 
+import java.util.Arrays;
+
 /**
  * @author bing
  */
@@ -10,7 +12,12 @@ public class LeetCode720 {
 
     public static void main(String[] args) {
         LeetCode720 leetCode720 = new LeetCode720();
-        System.out.println(leetCode720.longestWord(new String[]{"w", "wo", "wor", "worl", "world"}));
-        System.out.println(leetCode720.longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"}));
+//        System.out.println(leetCode720.longestWord(new String[]{"a", "b"}));
+//        System.out.println(leetCode720.longestWord(new String[]{"w", "wo", "wor", "worl", "world"}));
+//        System.out.println(leetCode720.longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"}));
+//        System.out.println(leetCode720.longestWord(new String[]{"b", "br", "bre", "brea", "break", "breakf", "breakfa", "breakfas", "breakfast", "l", "lu", "lun", "lunc", "lunch", "d", "di", "din", "dinn", "dinne", "dinner"}));
+//        System.out.println(leetCode720.longestWord(new String[]{"ogz", "eyj", "e", "ey", "hmn", "v", "hm", "ogznkb", "ogzn", "hmnm", "eyjuo", "vuq", "ogznk", "og", "eyjuoi", "d"}));
+
+        System.out.println(leetCode720.longestWord(new String[]{"rac", "rs", "ra", "on", "r", "otif", "o", "onpdu", "rsf", "rs", "ot", "oti", "racy", "onpd"}));
     }
 }
