3c9f0d3f3c76d8dec4cae6caddf0adec7ccf7436
zhengbinbin <zhengbinbin@58.com>
2021-06-02 00:08:54 +0800

    每日一题

---
+++ src/com/geekbing/easy/LeetCode459.java
@@ -0,0 +1,18 @@
+package com.geekbing.easy;
+
+/**
+ * @author bing
+ */
+public class LeetCode459 {
+    public boolean repeatedSubstringPattern(String s) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        LeetCode459 leetCode459 = new LeetCode459();
+
+        System.out.println(leetCode459.repeatedSubstringPattern("abab"));
+        System.out.println(leetCode459.repeatedSubstringPattern("aba"));
+        System.out.println(leetCode459.repeatedSubstringPattern("abcabcabcabc"));
+    }
+}
--- src/com/geekbing/easy/LeetCode703.java
+++ src/com/geekbing/easy/LeetCode703.java
@@ -1,26 +1,48 @@
 package com.geekbing.easy;
 
+import java.util.PriorityQueue;
+
 /**
  * @author bing
  */
 public class LeetCode703 {
     private static final class KthLargest {
+        private final PriorityQueue<Integer> queue;
+        private final int size;
 
         public KthLargest(int k, int[] nums) {
-
+            queue = new PriorityQueue<>(k);
+            size = k;
+            for (int num : nums) {
+                queue.offer(num);
+                if (queue.size() > k) {
+                    queue.poll();
+                }
+            }
         }
 
         public int add(int val) {
-            return 0;
+            queue.offer(val);
+            if (queue.size() > size) {
+                queue.poll();
+            }
+            return !queue.isEmpty() ? queue.peek() : -1;
         }
     }
 
     public static void main(String[] args) {
-        KthLargest kthLargest = new KthLargest(3, new int[]{4, 5, 8, 2});
-        kthLargest.add(3);   // return 4
-        kthLargest.add(5);   // return 5
-        kthLargest.add(10);  // return 5
-        kthLargest.add(9);   // return 8
-        kthLargest.add(4);   // return 8
+        KthLargest kthLargest1 = new KthLargest(3, new int[]{4, 5, 8, 2});
+        System.out.println(kthLargest1.add(3));   // return 4
+        System.out.println(kthLargest1.add(5));   // return 5
+        System.out.println(kthLargest1.add(10));  // return 5
+        System.out.println(kthLargest1.add(9));   // return 8
+        System.out.println(kthLargest1.add(4));   // return 8
+
+        KthLargest kthLargest2 = new KthLargest(1, new int[]{});
+        System.out.println(kthLargest2.add(-3));
+        System.out.println(kthLargest2.add(-2));
+        System.out.println(kthLargest2.add(-4));
+        System.out.println(kthLargest2.add(0));
+        System.out.println(kthLargest2.add(4));
     }
 }
