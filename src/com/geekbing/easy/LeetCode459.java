d965f13b4f4e5c29f28759b94d8673790fc2bbad
zhengbinbin <zhengbinbin@58.com>
2021-06-02 21:51:11 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode459.java
+++ src/com/geekbing/easy/LeetCode459.java
@@ -1,10 +1,43 @@
 package com.geekbing.easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author bing
  */
 public class LeetCode459 {
     public boolean repeatedSubstringPattern(String s) {
+        List<Integer> idxs = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[0]) {
+                idxs.add(i);
+            }
+        }
+        for (Integer idx : idxs) {
+            if (canReplace(chars, idx)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean canReplace(char[] chars, int len) {
+        if (chars.length % len != 0) {
+            return false;
+        }
+        int idx1 = 0, idx2 = len;
+        while (idx2 < chars.length) {
+            if (chars[idx1] != chars[idx2]) {
+                return false;
+            }
+            idx1++;
+            idx2++;
+            if (idx1 == len) {
+                idx1 = 0;
+            }
+        }
         return true;
     }
 
