c5071821ca558ef68bbf07649fc213b6c7a1551c
zhengbinbin <zhengbinbin@58.com>
2021-06-14 12:54:22 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode1248.java
+++ src/com/geekbing/middle/LeetCode1248.java
@@ -1,11 +1,27 @@
 package com.geekbing.middle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author bing
  */
 public class LeetCode1248 {
     public int numberOfSubarrays(int[] nums, int k) {
-        return 0;
+        int ans = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int count = 0;
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                count++;
+            }
+            if (map.containsKey(count - k)) {
+                ans += map.get(count - k);
+            }
+            map.put(count, map.getOrDefault(count, 0) + 1);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
