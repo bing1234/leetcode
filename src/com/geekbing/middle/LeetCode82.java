d7aca83799fa5b0eb5b44c39a19bd8d449640a99
zhengbinbin <zhengbinbin@58.com>
2021-06-20 14:21:09 +0800

    每日一题

---
+++ src/com/geekbing/middle/LeetCode82.java
@@ -0,0 +1,31 @@
+package com.geekbing.middle;
+
+/**
+ * @author bing
+ */
+public class LeetCode82 {
+    public ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private static final class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
