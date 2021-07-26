8e266a674f7ffaca84796ce24315696c2773e6cf
zhengbinbin <zhengbinbin@58.com>
2021-07-26 17:04:25 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode680.java
+++ src/com/geekbing/easy/LeetCode680.java
@@ -1,21 +1,56 @@
 package com.geekbing.easy;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author bing
  */
 public class LeetCode680 {
     public boolean validPalindrome(String s) {
         char[] chars = s.toCharArray();
-
+        int left = 0, right = chars.length - 1;
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                return isPalindrome(chars, left + 1, right) || isPalindrome(chars, left, right - 1);
+            }
+            left++;
+            right--;
+        }
         return true;
     }
 
-    public static void main(String[] args) {
+    private boolean isPalindrome(char[] chars, int left, int right) {
+        while (left < right) {
+            if (chars[left] != chars[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    @Test
+    public void testCase1() {
         LeetCode680 leetCode680 = new LeetCode680();
-        System.out.println(leetCode680.validPalindrome("a"));
-        System.out.println(leetCode680.validPalindrome("ab"));
-        System.out.println(leetCode680.validPalindrome("abc"));
-        System.out.println(leetCode680.validPalindrome("aba"));
-        System.out.println(leetCode680.validPalindrome("abca"));
+        assert leetCode680.validPalindrome("aba");
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode680 leetCode680 = new LeetCode680();
+        assert leetCode680.validPalindrome("abca");
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode680 leetCode680 = new LeetCode680();
+        assert !leetCode680.validPalindrome("abc");
+    }
+
+    @Test
+    public void testCase4() {
+        LeetCode680 leetCode680 = new LeetCode680();
+        assert !leetCode680.validPalindrome("bddb");
     }
 }
