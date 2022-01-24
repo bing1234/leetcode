20a4e08335eca2ef252d49f169226743ad069d44
zhengbinbin <zhengbinbin@58.com>
2022-01-24 12:35:24 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode1361.java
+++ src/com/geekbing/middle/LeetCode1361.java
@@ -2,14 +2,58 @@ package com.geekbing.middle;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * TODO
- *
  * @author bing
  */
 public class LeetCode1361 {
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        UnionFind unionFind = new UnionFind(n);
+        for (int i = 0; i < leftChild.length; i++) {
+            if (leftChild[i] != -1 && unionFind.union(i, leftChild[i])) {
+                return false;
+            }
+        }
+        for (int i = 0; i < rightChild.length; i++) {
+            if (rightChild[i] != -1 && unionFind.union(i, rightChild[i])) {
+                return false;
+            }
+        }
+        Set<Integer> roots = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            roots.add(unionFind.find(i));
+            if (roots.size() > 1) {
+                return false;
+            }
+        }
         return true;
+    }
+
+    private static class UnionFind {
+        private final int[] parent;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < parent.length; i++) {
+                parent[i] = i;
+            }
+        }
+
+        private int find(int i) {
+            return i == parent[i] ? i : find(parent[i]);
+        }
+
+        private boolean union(int i, int j) {
+            int rootI = find(i);
+            int rootJ = find(j);
+            if (rootI == rootJ) {
+                return true;
+            }
+            parent[j] = i;
+            return false;
+        }
     }
 
     @Test
@@ -34,5 +78,17 @@ public class LeetCode1361 {
     public void testCase4() {
         LeetCode1361 leetCode1361 = new LeetCode1361();
         assert !leetCode1361.validateBinaryTreeNodes(6, new int[]{1, -1, -1, 4, -1, -1}, new int[]{2, -1, -1, 5, -1, -1});
+    }
+
+    @Test
+    public void testCase5() {
+        LeetCode1361 leetCode1361 = new LeetCode1361();
+        assert !leetCode1361.validateBinaryTreeNodes(3, new int[]{1, -1, -1}, new int[]{-1, -1, 1});
+    }
+
+    @Test
+    public void testCase6() {
+        LeetCode1361 leetCode1361 = new LeetCode1361();
+        assert leetCode1361.validateBinaryTreeNodes(4, new int[]{3, -1, 1, -1}, new int[]{-1, -1, 0, -1});
     }
 }
