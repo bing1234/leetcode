ec86ce2621942187ca2dcf6b4133b15e844d02be
zhengbinbin <zhengbinbin@58.com>
2022-01-26 16:02:57 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode785.java
+++ src/com/geekbing/middle/LeetCode785.java
@@ -2,28 +2,44 @@ package com.geekbing.middle;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * @author bing
  */
 public class LeetCode785 {
     public boolean isBipartite(int[][] graph) {
+        UnionFind unionFind = new UnionFind(graph.length);
+        for (int i = 0; i < graph.length; i++) {
+            for (int num : graph[i]) {
+                if (unionFind.isConnected(i, num)) {
+                    return false;
+                }
+                unionFind.union(graph[i][0], num);
+            }
+        }
         return true;
     }
 
     private static class UnionFind {
-        private int[] parent;
-        private int[] weight;
+        private final int[] parent;
+        private final int[] weight;
 
-        public UnionFind() {
-//            this.parent = new int[];
-//            this.weight = new int[];
+        public UnionFind(int n) {
+            parent = new int[n];
+            weight = new int[n];
             for (int i = 0; i < parent.length; i++) {
                 parent[i] = i;
             }
+            Arrays.fill(weight, 1);
         }
 
         private int find(int i) {
             return i == parent[i] ? i : find(parent[i]);
+        }
+
+        private boolean isConnected(int i, int j) {
+            return find(i) == find(j);
         }
 
         private void union(int i, int j) {
@@ -51,5 +67,11 @@ public class LeetCode785 {
     public void testCase2() {
         LeetCode785 leetCode785 = new LeetCode785();
         assert leetCode785.isBipartite(new int[][]{{1, 3}, {0, 2}, {1, 3}, {0, 2}});
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode785 leetCode785 = new LeetCode785();
+        assert leetCode785.isBipartite(new int[][]{{3, 4, 6}, {3, 6}, {3, 6}, {0, 1, 2, 5}, {0, 7, 8}, {3}, {0, 1, 2, 7}, {4, 6}, {4}, {}});
     }
 }
