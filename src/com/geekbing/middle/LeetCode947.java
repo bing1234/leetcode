10fa4faf08889b3205cb5121406fa041d3704e91
zhengbinbin <zhengbinbin@58.com>
2022-01-27 13:03:31 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode947.java
+++ src/com/geekbing/middle/LeetCode947.java
@@ -2,14 +2,61 @@ package com.geekbing.middle;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * TODO
- *
  * @author bing
  */
 public class LeetCode947 {
     public int removeStones(int[][] stones) {
-        return 0;
+        UnionFind unionFind = new UnionFind(stones.length);
+        for (int i = 0; i < stones.length; i++) {
+            for (int j = i + 1; j < stones.length; j++) {
+                // 同一行或同一列
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    unionFind.union(i, j);
+                }
+            }
+        }
+        Set<Integer> roots = new HashSet<>();
+        for (int i = 0; i < stones.length; i++) {
+            roots.add(unionFind.find(i));
+        }
+        return stones.length - roots.size();
+    }
+
+    private static class UnionFind {
+        private final int[] parent;
+        private final int[] weight;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            weight = new int[n];
+            for (int i = 0; i < parent.length; i++) {
+                parent[i] = i;
+            }
+            Arrays.fill(weight, 1);
+        }
+
+        private int find(int i) {
+            return i == parent[i] ? i : find(parent[i]);
+        }
+
+        private void union(int i, int j) {
+            int rootI = find(i);
+            int rootJ = find(j);
+            if (rootI != rootJ) {
+                if (weight[rootI] < weight[rootJ]) {
+                    parent[rootI] = rootJ;
+                    weight[rootJ] += weight[rootI];
+                } else {
+                    parent[rootJ] = rootI;
+                    weight[rootI] += weight[rootJ];
+                }
+            }
+        }
     }
 
     @Test
