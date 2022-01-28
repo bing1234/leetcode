6bb3bf2a643fbe5fa407bf6928c872e224a5d703
zhengbinbin <zhengbinbin@58.com>
2022-01-28 11:32:34 +0800

    每日一题

--- src/com/geekbing/LeetCode1971.java
+++ src/com/geekbing/LeetCode1971.java
@@ -2,12 +2,54 @@ package com.geekbing;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * @author bing
  */
 public class LeetCode1971 {
-    public boolean validPath(int n, int[][] edges, int start, int end) {
-        return false;
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        UnionFind unionFind = new UnionFind(n);
+        for (int[] edge : edges) {
+            unionFind.union(edge[0], edge[1]);
+        }
+        return unionFind.isConnected(source, destination);
+    }
+
+    private static class UnionFind {
+        private final int[] parent;
+        private final int[] weight;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            weight = new int[n];
+            for (int i = 0; i < parent.length; i++) {
+                parent[i] = i;
+            }
+            Arrays.fill(weight, 1);
+        }
+
+        private int find(int i) {
+            return i == parent[i] ? i : find(parent[i]);
+        }
+
+        private boolean isConnected(int i, int j) {
+            return find(i) == find(j);
+        }
+
+        private void union(int i, int j) {
+            int rootI = find(i);
+            int rootJ = find(j);
+            if (rootI != rootJ) {
+                if (weight[rootI] < weight[rootJ]) {
+                    parent[rootI] = rootJ;
+                    weight[rootJ] += weight[rootI];
+                } else {
+                    parent[rootJ] = rootI;
+                    weight[rootI] += weight[rootJ];
+                }
+            }
+        }
     }
 
     @Test
