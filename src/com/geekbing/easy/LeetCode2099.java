6b150d1a22697b1dbf02ad85af33882201bf3cae
zhengbinbin <zhengbinbin@58.com>
2022-01-29 23:27:30 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode2099.java
+++ src/com/geekbing/easy/LeetCode2099.java
@@ -1,10 +1,9 @@
-package com.geekbing;
+package com.geekbing.easy;
 
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
