daa890555bc9ff16304e5117ba1254b4561b3a3c
zhengbinbin <zhengbinbin@58.com>
2022-02-16 21:55:16 +0800

    每日一题

---
+++ src/com/geekbing/LeetCode450.java
@@ -0,0 +1,29 @@
+package com.geekbing;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * todo
+ *
+ * @author bing
+ */
+public class LeetCode450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return null;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    @Test
+    public void testCase1() {
+        LeetCode450 leetCode450 = new LeetCode450();
+    }
+}
