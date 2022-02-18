a16f18c6094bfa62a7c26d36fe678b4e1a51d860
zhengbinbin <zhengbinbin@58.com>
2022-02-18 20:25:19 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode1111.java
+++ src/com/geekbing/middle/LeetCode1111.java
@@ -1,18 +1,73 @@
 package com.geekbing.middle;
 
-import java.util.Arrays;
+import org.junit.jupiter.api.Test;
 
 /**
- * todo
+ * @author bing
  */
 public class LeetCode1111 {
     public int[] maxDepthAfterSplit(String seq) {
-        return new int[]{};
+        int[] ans = new int[seq.length()];
+        int cnt = 0;
+        for (int i = 0; i < seq.length(); i++) {
+            if (seq.charAt(i) == '(') {
+                cnt++;
+                ans[i] = cnt % 2;
+            } else {
+                ans[i] = cnt % 2;
+                cnt--;
+            }
+        }
+        return ans;
     }
 
-    public static void main(String[] args) {
-        LeetCode1111 leetCode1111 = new LeetCode1111();
-        System.out.println(Arrays.toString(leetCode1111.maxDepthAfterSplit("(()())")));
-        System.out.println(Arrays.toString(leetCode1111.maxDepthAfterSplit("()(())()")));
+    @Test
+    public void testCase1() {
+        LeetCode1111 leetCode410 = new LeetCode1111();
+        String seq = "(()())";
+
+        int[] ans = leetCode410.maxDepthAfterSplit(seq);
+
+        String a = seplitStr(seq, ans, 0);
+        String b = seplitStr(seq, ans, 1);
+
+        assert Math.abs(depth(a) - depth(b)) <= 1;
+    }
+
+
+    @Test
+    public void testCase2() {
+        LeetCode1111 leetCode410 = new LeetCode1111();
+
+        String seq = "()(())()";
+        int[] ans = leetCode410.maxDepthAfterSplit(seq);
+
+        String a = seplitStr(seq, ans, 0);
+        String b = seplitStr(seq, ans, 1);
+
+        assert Math.abs(depth(a) - depth(b)) <= 1;
+    }
+
+    private int depth(String str) {
+        int ans = 0, cnt = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                cnt++;
+                ans = Math.max(ans, cnt);
+            } else {
+                cnt--;
+            }
+        }
+        return ans;
+    }
+
+    private String seplitStr(String str, int[] idx, int num) {
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < idx.length; i++) {
+            if (idx[i] == num) {
+                ans.append(str.charAt(i));
+            }
+        }
+        return ans.toString();
     }
 }
