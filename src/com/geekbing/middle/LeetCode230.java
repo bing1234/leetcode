aa8102127077dc30b7d76f3031655af7c101e279
zhengbinbin <zhengbinbin@58.com>
2022-02-18 12:51:16 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode230.java
+++ src/com/geekbing/middle/LeetCode230.java
@@ -1,13 +1,30 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author bing
  */
 public class LeetCode230 {
     public int kthSmallest(TreeNode root, int k) {
+        int leftCount = countTree(root.left);
+        if (leftCount < k - 1) {
+            return kthSmallest(root.right, k - leftCount - 1);
+        } else if (leftCount == k - 1) {
+            return root.val;
+        } else {
+            return kthSmallest(root.left, k);
+        }
+    }
 
-
-        return 0;
+    private int countTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return 1 + countTree(root.left) + countTree(root.right);
     }
 
     private static final class TreeNode {
@@ -15,17 +32,30 @@ public class LeetCode230 {
         TreeNode left;
         TreeNode right;
 
-        TreeNode() {
-        }
-
         TreeNode(int val) {
             this.val = val;
         }
+    }
 
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+    @Test
+    public void testCase1() {
+        LeetCode230 leetCode230 = new LeetCode230();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.left.right = new TreeNode(2);
+        root.right = new TreeNode(4);
+        assert leetCode230.kthSmallest(root, 1) == 1;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode230 leetCode230 = new LeetCode230();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(2);
+        root.left.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(4);
+        root.right = new TreeNode(6);
+        assert leetCode230.kthSmallest(root, 3) == 3;
     }
 }
