47ed39e6992d255c7d01c7869fb6e3ea00976116
zhengbinbin <zhengbinbin@58.com>
2022-02-22 00:47:05 +0800

    每日一题

---
+++ src/com/geekbing/middle/LeetCode1438.java
@@ -0,0 +1,30 @@
+package com.geekbing.middle;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bing
+ */
+public class LeetCode1438 {
+    public int longestSubarray(int[] nums, int limit) {
+        return 0;
+    }
+
+    @Test
+    public void testCase1() {
+        LeetCode1438 leetCode1438 = new LeetCode1438();
+        assert leetCode1438.longestSubarray(new int[]{8, 2, 4, 7}, 4) == 2;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode1438 leetCode1438 = new LeetCode1438();
+        assert leetCode1438.longestSubarray(new int[]{10, 1, 2, 4, 7, 2}, 5) == 4;
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode1438 leetCode1438 = new LeetCode1438();
+        assert leetCode1438.longestSubarray(new int[]{4, 2, 2, 2, 4, 4, 2, 2}, 0) == 3;
+    }
+}
