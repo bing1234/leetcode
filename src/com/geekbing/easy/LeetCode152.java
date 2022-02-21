7922d1503e360c61321223f7d017ab027e095a84
zhengbinbin <zhengbinbin@58.com>
2022-02-21 23:54:04 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode152.java
+++ src/com/geekbing/easy/LeetCode152.java
@@ -1,27 +1,33 @@
 package com.geekbing.easy;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author bing
  */
 public class LeetCode152 {
     public int maxProduct(int[] nums) {
         // 以当前元素结尾的乘积最大、最小子数组
-        int preMax = nums[0], preMin = nums[0], max = nums[0], currentMax, currentMin;
+        int preMax = nums[0], preMin = nums[0], ans = nums[0], currentMax, currentMin;
         for (int i = 1; i < nums.length; i++) {
             currentMax = Math.max(nums[i], Math.max(nums[i] * preMin, nums[i] * preMax));
             currentMin = Math.min(nums[i], Math.min(nums[i] * preMin, nums[i] * preMax));
-            if (currentMax > max) {
-                max = currentMax;
-            }
+            ans = Math.max(ans, currentMax);
             preMax = currentMax;
             preMin = currentMin;
         }
-        return max;
+        return ans;
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void testCase1() {
         LeetCode152 leetCode152 = new LeetCode152();
-        System.out.println(leetCode152.maxProduct(new int[]{2, 3, -2, 4}));
-        System.out.println(leetCode152.maxProduct(new int[]{-2, 0, -1}));
+        assert leetCode152.maxProduct(new int[]{2, 3, -2, 4}) == 6;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode152 leetCode152 = new LeetCode152();
+        assert leetCode152.maxProduct(new int[]{-2, 0, -1}) == 0;
     }
 }
