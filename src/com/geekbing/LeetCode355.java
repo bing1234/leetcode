0d8903fa00786428b9fe5290d0e765aae02eacb2
zhengbinbin <zhengbinbin@58.com>
2022-02-22 21:15:27 +0800

    每日一题

---
+++ src/com/geekbing/LeetCode355.java
@@ -0,0 +1,61 @@
+package com.geekbing;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author bing
+ */
+public class LeetCode355 {
+    private static class Twitter {
+
+        /**
+         * Initialize your data structure here.
+         */
+        public Twitter() {
+
+        }
+
+        /**
+         * Compose a new tweet.
+         */
+        public void postTweet(int userId, int tweetId) {
+
+        }
+
+        /**
+         * Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+         */
+        public List<Integer> getNewsFeed(int userId) {
+            return new ArrayList<>();
+        }
+
+        /**
+         * Follower follows a followee. If the operation is invalid, it should be a no-op.
+         */
+        public void follow(int followerId, int followeeId) {
+
+        }
+
+        /**
+         * Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+         */
+        public void unfollow(int followerId, int followeeId) {
+
+        }
+    }
+
+    @Test
+    public void testCase1() {
+        Twitter twitter = new Twitter();
+        twitter.postTweet(1, 5); // 用户 1 发送了一条新推文 (用户 id = 1, 推文 id = 5)
+        twitter.getNewsFeed(1);  // 用户 1 的获取推文应当返回一个列表，其中包含一个 id 为 5 的推文
+        twitter.follow(1, 2);    // 用户 1 关注了用户 2
+        twitter.postTweet(2, 6); // 用户 2 发送了一个新推文 (推文 id = 6)
+        twitter.getNewsFeed(1);  // 用户 1 的获取推文应当返回一个列表，其中包含两个推文，id 分别为 -> [6, 5] 。推文 id 6 应当在推文 id 5 之前，因为它是在 5 之后发送的
+        twitter.unfollow(1, 2);  // 用户 1 取消关注了用户 2
+        twitter.getNewsFeed(1);  // 用户 1 获取推文应当返回一个列表，其中包含一个 id 为 5 的推文。因为用户 1 已经不再关注用户 2
+    }
+}
