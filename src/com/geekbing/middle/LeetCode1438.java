79d0e3a569f8ec36254ae181609b6b2221dcde56
zhengbinbin <zhengbinbin@58.com>
2022-02-22 21:51:28 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode1438.java
+++ src/com/geekbing/middle/LeetCode1438.java
@@ -2,12 +2,27 @@ package com.geekbing.middle;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.TreeMap;
+
 /**
  * @author bing
  */
 public class  LeetCode1438 {
     public int longestSubarray(int[] nums, int limit) {
-        return 0;
+        int left = 0, ans = 0;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int right = 0; right < nums.length; right++) {
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            while (map.lastKey() - map.firstKey() > limit) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
     }
 
     @Test
@@ -26,5 +41,11 @@ public class  LeetCode1438 {
     public void testCase3() {
         LeetCode1438 leetCode1438 = new LeetCode1438();
         assert leetCode1438.longestSubarray(new int[]{4, 2, 2, 2, 4, 4, 2, 2}, 0) == 3;
+    }
+
+    @Test
+    public void testCase4() {
+        LeetCode1438 leetCode1438 = new LeetCode1438();
+        assert leetCode1438.longestSubarray(new int[]{8}, 10) == 1;
     }
 }
--- src/com/geekbing/middle/LeetCode221.java
+++ src/com/geekbing/middle/LeetCode221.java
@@ -1,20 +1,55 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author bing
  */
 public class LeetCode221 {
     public int maximalSquare(char[][] matrix) {
-        int[][] dp = new int[matrix.length][matrix[0].length];
-
-        return 0;
+        int side = 0, row = matrix.length, col = matrix[0].length;
+        // 令dp[i][j]表示以[i][j]为右下角的最大正方形的边长
+        int[][] dp = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            dp[i][0] = matrix[i][0] == '1' ? 1 : 0;
+            side = Math.max(side, dp[i][0]);
+        }
+        for (int j = 0; j < col; j++) {
+            dp[0][j] = matrix[0][j] == '1' ? 1 : 0;
+            side = Math.max(side, dp[0][j]);
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+                side = Math.max(side, dp[i][j]);
+            }
+        }
+        return side * side;
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void testCase1() {
         LeetCode221 leetCode221 = new LeetCode221();
+        int ans = leetCode221.maximalSquare(new char[][]{
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'}
+        });
+        assert ans == 4;
+    }
 
-        System.out.println(leetCode221.maximalSquare(new char[][]{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}}));
-        System.out.println(leetCode221.maximalSquare(new char[][]{{'0', '1'}, {'1', '0'}}));
-        System.out.println(leetCode221.maximalSquare(new char[][]{{'0'}}));
+    @Test
+    public void testCase2() {
+        LeetCode221 leetCode221 = new LeetCode221();
+        int ans = leetCode221.maximalSquare(new char[][]{
+                {'0', '1'},
+                {'1', '0'}
+        });
+        assert ans == 1;
     }
 }
