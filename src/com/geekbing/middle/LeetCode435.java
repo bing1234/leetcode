b0036bdab3e5520664d7af7f48c0634730aeb234
zhengbinbin <zhengbinbin@58.com>
2022-02-23 22:48:38 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode435.java
+++ src/com/geekbing/middle/LeetCode435.java
@@ -1,15 +1,42 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
- * todo
+ * @author bing
  */
 public class LeetCode435 {
     public int eraseOverlapIntervals(int[][] intervals) {
-        return 0;
+        Arrays.sort(intervals, Comparator.comparing(o -> o[1]));
+        int ans = 0, preEnd = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < preEnd) {
+                ans++;
+            } else {
+                preEnd = intervals[i][1];
+            }
+        }
+        return ans;
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void testCase1() {
         LeetCode435 leetCode435 = new LeetCode435();
-        System.out.println(leetCode435.eraseOverlapIntervals(new int[][]{}));
+        assert leetCode435.eraseOverlapIntervals(new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 3}}) == 1;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode435 leetCode435 = new LeetCode435();
+        assert leetCode435.eraseOverlapIntervals(new int[][]{{1, 2}, {1, 2}, {1, 2}}) == 2;
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode435 leetCode435 = new LeetCode435();
+        assert leetCode435.eraseOverlapIntervals(new int[][]{{1, 2}, {2, 3}}) == 0;
     }
 }
--- src/com/geekbing/middle/LeetCode452.java
+++ src/com/geekbing/middle/LeetCode452.java
@@ -1,31 +1,60 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * @author bing
+ */
 public class LeetCode452 {
     public int findMinArrowShots(int[][] points) {
-        if (points.length == 0) {
-            return 0;
-        }
-        Arrays.sort(points, Comparator.comparingInt(p -> p[0]));
-        int ans = 1;
-        int right = points[0][1];
+        Arrays.sort(points, Comparator.comparingInt(o -> o[0]));
+        int ans = 1, preEnd = points[0][1];
         for (int i = 1; i < points.length; i++) {
-            if (points[i][0] > right) {
+            if (points[i][0] > preEnd) {
                 ans++;
-                right = points[i][1];
+                preEnd = points[i][1];
             } else {
-                if (points[i][1] < right) {
-                    right = points[i][1];
-                }
+                preEnd = Math.min(preEnd, points[i][1]);
             }
         }
         return ans;
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void testCase1() {
         LeetCode452 leetCode452 = new LeetCode452();
-        System.out.println(leetCode452.findMinArrowShots(new int[][]{{10, 16}, {2, 8}, {1, 6}, {7, 12}}));
+        int ans = leetCode452.findMinArrowShots(new int[][]{{10, 16}, {2, 8}, {1, 6}, {7, 12}});
+        assert ans == 2;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode452 leetCode452 = new LeetCode452();
+        int ans = leetCode452.findMinArrowShots(new int[][]{{1, 2}, {3, 4}, {5, 6}, {7, 8}});
+        assert ans == 4;
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode452 leetCode452 = new LeetCode452();
+        int ans = leetCode452.findMinArrowShots(new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}});
+        assert ans == 2;
+    }
+
+    @Test
+    public void testCase4() {
+        LeetCode452 leetCode452 = new LeetCode452();
+        int ans = leetCode452.findMinArrowShots(new int[][]{{1, 2}});
+        assert ans == 1;
+    }
+
+    @Test
+    public void testCase5() {
+        LeetCode452 leetCode452 = new LeetCode452();
+        int ans = leetCode452.findMinArrowShots(new int[][]{{2, 3}, {2, 3}});
+        assert ans == 1;
     }
 }
