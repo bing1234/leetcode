390f088842a628c92c729ee21dbee8e6e057cb38
zhengbinbin <zhengbinbin@58.com>
2022-03-01 00:00:29 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode264.java
+++ src/com/geekbing/middle/LeetCode264.java
@@ -1,11 +1,36 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author bing
  */
 public class LeetCode264 {
+    public int nthUglyNumberV2(int n) {
+        // dp[i]表示第i个整数
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        // p2、p3、p5 分别表示有资格同2、3、5相乘的最小下标i的位置
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = Math.min(num2, Math.min(num3, num5));
+            if (dp[i] == num2) {
+                p2++;
+            }
+            if (dp[i] == num3) {
+                p3++;
+            }
+            if (dp[i] == num5) {
+                p5++;
+            }
+        }
+        return dp[n];
+    }
+
     public int nthUglyNumber(int n) {
         // 丑数就是质因数只包含 2, 3, 5 的正整数
+        // 令 dp[i]表示第i个丑数
         long[] dp = new long[n + 5];
         dp[1] = 1;
         dp[2] = 2;
@@ -15,14 +40,14 @@ public class LeetCode264 {
         for (int i = 6; i <= n; i++) {
             long min = Integer.MAX_VALUE;
             for (int j = 1; j < i; j++) {
-                if (dp[j] * 2 > dp[i - 1] && dp[j] * 2 < min) {
-                    min = dp[j] * 2;
+                if (dp[j] * 2 > dp[i - 1]) {
+                    min = Math.min(min, dp[j] * 2);
                 }
-                if (dp[j] * 3 > dp[i - 1] && dp[j] * 3 < min) {
-                    min = dp[j] * 3;
+                if (dp[j] * 3 > dp[i - 1]) {
+                    min = Math.min(min, dp[j] * 3);
                 }
-                if (dp[j] * 5 > dp[i - 1] && dp[j] * 5 < min) {
-                    min = dp[j] * 5;
+                if (dp[j] * 5 > dp[i - 1]) {
+                    min = Math.min(min, dp[j] * 5);
                 }
             }
             dp[i] = min;
@@ -30,14 +55,27 @@ public class LeetCode264 {
         return (int) dp[n];
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void testCase1() {
         LeetCode264 leetCode264 = new LeetCode264();
-        System.out.println(leetCode264.nthUglyNumber(1));
-        System.out.println(leetCode264.nthUglyNumber(2));
-        System.out.println(leetCode264.nthUglyNumber(3));
-        System.out.println(leetCode264.nthUglyNumber(4));
-        System.out.println(leetCode264.nthUglyNumber(5));
-        System.out.println(leetCode264.nthUglyNumber(10));
-        System.out.println(leetCode264.nthUglyNumber(1690));
+        assert leetCode264.nthUglyNumber(10) == 12;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode264 leetCode264 = new LeetCode264();
+        assert leetCode264.nthUglyNumber(1) == 1;
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode264 leetCode264 = new LeetCode264();
+        assert leetCode264.nthUglyNumberV2(10) == 12;
+    }
+
+    @Test
+    public void testCase4() {
+        LeetCode264 leetCode264 = new LeetCode264();
+        assert leetCode264.nthUglyNumberV2(1) == 1;
     }
 }
--- src/com/geekbing/middle/LeetCode313.java
+++ src/com/geekbing/middle/LeetCode313.java
@@ -1,5 +1,7 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 
 /**
@@ -7,25 +9,38 @@ import java.util.Arrays;
  */
 public class LeetCode313 {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        long[] dp = new long[n + 1];
+        int[] dp = new int[n + 1];
         dp[1] = 1;
+        // p2、p3、p5 分别表示有资格同2、3、5相乘的最小下标i的位置
+        int[] p = new int[primes.length];
+        Arrays.fill(p, 1);
         for (int i = 2; i <= n; i++) {
-            long min = Integer.MAX_VALUE;
-            for (int j = 1; j < i; j++) {
-                for (int prime : primes) {
-                    if (dp[j] * prime > dp[i - 1] && dp[j] * prime < min) {
-                        min = dp[j] * prime;
-                    }
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < primes.length; j++) {
+                if (dp[p[j]] * primes[j] > dp[i - 1]) {
+                    min = Math.min(min, dp[p[j]] * primes[j]);
                 }
             }
             dp[i] = min;
+            for (int j = 0; j < primes.length; j++) {
+                if (min == dp[p[j]] * primes[j]) {
+                    p[j]++;
+                }
+            }
         }
-        return (int) dp[n];
+        return dp[n];
     }
 
-    public static void main(String[] args) {
-        LeetCode313 leetCode313 = new LeetCode313();
 
-        System.out.println(leetCode313.nthSuperUglyNumber(12, new int[]{2, 7, 13, 19}));
+    @Test
+    public void testCase1() {
+        LeetCode313 leetCode313 = new LeetCode313();
+        assert leetCode313.nthSuperUglyNumber(12, new int[]{2, 7, 13, 19}) == 32;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode313 leetCode313 = new LeetCode313();
+        assert leetCode313.nthSuperUglyNumber(1, new int[]{2, 3, 5}) == 1;
     }
 }
