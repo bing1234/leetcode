a19ce0a8b2162c0401cecf20030904807cee8ea4
zhengbinbin <zhengbinbin@58.com>
2022-03-01 23:55:00 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode650.java
+++ src/com/geekbing/middle/LeetCode650.java
@@ -10,18 +10,19 @@ public class LeetCode650 {
         if (n == 1) {
             return 0;
         }
-        if (n == 2) {
-            return 2;
-        }
-        if (n % 2 != 0) {
-            return n;
-        }
-        int ans = Integer.MAX_VALUE;
-        for (int i = 3; i < n; i++) {
-            if (n % i == 0) {
+        int[] dp = new int[n + 1];
+        dp[1] = 0;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    min = Math.min(min, dp[j] + i / j);
+                }
             }
+            dp[i] = min;
         }
-        return ans;
+        return dp[n];
     }
 
     @Test
