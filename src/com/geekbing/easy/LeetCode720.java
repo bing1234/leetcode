42b8182a8ee3aaf0cbec0283c4a4836decb5100b
zhengbinbin <zhengbinbin@58.com>
2022-03-02 20:11:09 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode720.java
+++ src/com/geekbing/easy/LeetCode720.java
@@ -1,23 +1,49 @@
 package com.geekbing.easy;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author bing
  */
 public class LeetCode720 {
     public String longestWord(String[] words) {
-        return "";
+        Set<String> set = Arrays.stream(words).collect(Collectors.toSet());
+        String ans = "";
+        for (String word : words) {
+            boolean flag = true;
+            for (int i = 1; i <= word.length(); i++) {
+                if (!set.contains(word.substring(0, i))) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (!flag || word.length() < ans.length()) {
+                continue;
+            }
+            if (word.length() > ans.length()) {
+                ans = word;
+            } else {
+                if (word.compareTo(ans) < 0) {
+                    ans = word;
+                }
+            }
+        }
+        return ans;
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void testCase1() {
         LeetCode720 leetCode720 = new LeetCode720();
-//        System.out.println(leetCode720.longestWord(new String[]{"a", "b"}));
-//        System.out.println(leetCode720.longestWord(new String[]{"w", "wo", "wor", "worl", "world"}));
-//        System.out.println(leetCode720.longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"}));
-//        System.out.println(leetCode720.longestWord(new String[]{"b", "br", "bre", "brea", "break", "breakf", "breakfa", "breakfas", "breakfast", "l", "lu", "lun", "lunc", "lunch", "d", "di", "din", "dinn", "dinne", "dinner"}));
-//        System.out.println(leetCode720.longestWord(new String[]{"ogz", "eyj", "e", "ey", "hmn", "v", "hm", "ogznkb", "ogzn", "hmnm", "eyjuo", "vuq", "ogznk", "og", "eyjuoi", "d"}));
+        assert leetCode720.longestWord(new String[]{"w", "wo", "wor", "worl", "world"}).equals("world");
+    }
 
-        System.out.println(leetCode720.longestWord(new String[]{"rac", "rs", "ra", "on", "r", "otif", "o", "onpdu", "rsf", "rs", "ot", "oti", "racy", "onpd"}));
+    @Test
+    public void testCase2() {
+        LeetCode720 leetCode720 = new LeetCode720();
+        assert leetCode720.longestWord(new String[]{"a", "banana", "app", "appl", "ap", "apply", "apple"}).equals("apple");
     }
 }
