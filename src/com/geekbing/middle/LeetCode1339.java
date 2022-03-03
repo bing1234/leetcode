f5874617e64a75772fd15892ac822eaf90a5d53c
zhengbinbin <zhengbinbin@58.com>
2022-03-03 09:37:18 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode1339.java
+++ src/com/geekbing/middle/LeetCode1339.java
@@ -1,87 +1,40 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
 /**
- * todo
- *
  * @author bing
  */
 public class LeetCode1339 {
+    private long total;
+    private long ans;
+
     public int maxProduct(TreeNode root) {
         if (root == null) {
             return 0;
         }
         // 所有和
-        long total = sumOfTree(root);
+        total = sumOfTree(root);
         // 遍历二叉树
-        return (int) (travelTree(root, total) % 1000000007L);
+        travelTree(root);
+        return (int) (ans % 1000000007L);
     }
 
-    private long travelTree(TreeNode root, long total) {
+    private long travelTree(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        long max = 0;
-        if (root.left != null) {
-            long leftSum = sumOfTree(root.left);
-            max = Math.max(max, leftSum * (total - leftSum));
-            max = Math.max(max, travelTree(root.left, total));
-        }
-        if (root.right != null) {
-            long rightSum = sumOfTree(root.right);
-            max = Math.max(max, rightSum * (total - rightSum));
-            max = Math.max(max, travelTree(root.right, total));
-        }
-        return max;
+        // 计算当前节点以及左右子数的和
+        long curSum = root.val + travelTree(root.left) + travelTree(root.right);
+        ans = Math.max(ans, curSum * (total - curSum));
+        return curSum;
     }
 
     private long sumOfTree(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        long sum = root.val;
-        if (root.left != null) {
-            sum = sum + sumOfTree(root.left);
-        }
-        if (root.right != null) {
-            sum = sum + sumOfTree(root.right);
-        }
-        return sum;
-    }
-
-    public static void main(String[] args) {
-        LeetCode1339 leetCode1339 = new LeetCode1339();
-        TreeNode case1 = new TreeNode(1);
-        case1.left = new TreeNode(2);
-        case1.left.left = new TreeNode(4);
-        case1.left.right = new TreeNode(5);
-        case1.right = new TreeNode(3);
-        case1.right.left = new TreeNode(6);
-        System.out.println(leetCode1339.maxProduct(case1));
-
-        TreeNode case2 = new TreeNode(1);
-        case2.right = new TreeNode(2);
-        case2.right.left = new TreeNode(3);
-        case2.right.right = new TreeNode(4);
-        case2.right.right.left = new TreeNode(5);
-        case2.right.right.right = new TreeNode(6);
-        System.out.println(leetCode1339.maxProduct(case2));
-
-        TreeNode case3 = new TreeNode(2);
-        case3.left = new TreeNode(3);
-        case3.left.left = new TreeNode(10);
-        case3.left.left.left = new TreeNode(5);
-        case3.left.left.right = new TreeNode(4);
-        case3.left.right = new TreeNode(7);
-        case3.left.right.left = new TreeNode(11);
-        case3.left.right.right = new TreeNode(1);
-        case3.right = new TreeNode(9);
-        case3.right.left = new TreeNode(8);
-        case3.right.right = new TreeNode(6);
-        System.out.println(leetCode1339.maxProduct(case3));
-
-        TreeNode case4 = new TreeNode(1);
-        case4.left = new TreeNode(1);
-        System.out.println(leetCode1339.maxProduct(case4));
+        return root.val + sumOfTree(root.left) + sumOfTree(root.right);
     }
 
     private static class TreeNode {
@@ -92,5 +45,66 @@ public class LeetCode1339 {
         TreeNode(int x) {
             val = x;
         }
+    }
+
+    @Test
+    public void testCase1() {
+        LeetCode1339 leetCode1339 = new LeetCode1339();
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+
+        int ans = leetCode1339.maxProduct(root);
+        assert ans == 110;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode1339 leetCode1339 = new LeetCode1339();
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        root.right.right = new TreeNode(4);
+        root.right.right.left = new TreeNode(5);
+        root.right.right.right = new TreeNode(6);
+
+        int ans = leetCode1339.maxProduct(root);
+        assert ans == 90;
+    }
+
+    @Test
+    public void testCase3(){
+        LeetCode1339 leetCode1339 = new LeetCode1339();
+
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(10);
+        root.left.left.left = new TreeNode(5);
+        root.left.left.right = new TreeNode(4);
+        root.left.right = new TreeNode(7);
+        root.left.right.left = new TreeNode(11);
+        root.left.right.right = new TreeNode(1);
+        root.right = new TreeNode(9);
+        root.right.left = new TreeNode(8);
+        root.right.right = new TreeNode(6);
+
+        int ans = leetCode1339.maxProduct(root);
+        assert ans == 1025;
+    }
+
+    @Test
+    public void testCase4(){
+        LeetCode1339 leetCode1339 = new LeetCode1339();
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(1);
+
+        int ans = leetCode1339.maxProduct(root);
+        assert ans == 1;
     }
 }
