e110d78376e8ce5bdd8cfb84bba4fc3746aa6895
zhengbinbin <zhengbinbin@58.com>
2022-03-04 17:00:09 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode220.java
+++ src/com/geekbing/middle/LeetCode220.java
@@ -2,21 +2,47 @@ package com.geekbing.middle;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * todo 超时
- *
  * @author bing
  */
 public class LeetCode220 {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        for (int left = 0; left < nums.length; left++) {
-            for (int right = left + 1; right <= Math.min(left + k, nums.length - 1); right++) {
-                if (Math.abs((long) nums[left] - (long) nums[right]) <= t) {
-                    return true;
-                }
+        // 桶的下标 - 桶中的元素
+        Map<Long, Long> bucket = new HashMap<>();
+
+        int bucketWidth = t + 1;
+        for (int i = 0; i < nums.length; i++) {
+            // 获取需要插入到哪个桶中
+            long bucketIdx = getBucketIdx(nums[i], bucketWidth);
+            if (bucket.containsKey(bucketIdx)) {
+                return true;
+            }
+            // 比较左边的
+            if (bucket.containsKey(bucketIdx - 1) && Math.abs(bucket.get(bucketIdx - 1) - nums[i]) <= t) {
+                return true;
+            }
+            // 比较右边的
+            if (bucket.containsKey(bucketIdx + 1) && Math.abs(bucket.get(bucketIdx + 1) - nums[i]) <= t) {
+                return true;
+            }
+            bucket.put(bucketIdx, (long) nums[i]);
+
+            // 可以剔除之前的元素了
+            if (i >= k) {
+                bucket.remove(getBucketIdx(nums[i - k], bucketWidth));
             }
         }
         return false;
+    }
+
+    private long getBucketIdx(long num, int bucketWidth) {
+        if (num >= 0) {
+            return num / bucketWidth;
+        }
+        return (num + 1) / bucketWidth - 1;
     }
 
     @Test
@@ -41,5 +67,18 @@ public class LeetCode220 {
     public void testCase4() {
         LeetCode220 leetCode220 = new LeetCode220();
         assert !leetCode220.containsNearbyAlmostDuplicate(new int[]{-2147483648, 2147483647}, 1, 1);
+    }
+
+    @Test
+    public void testCase5() {
+        LeetCode220 leetCode220 = new LeetCode220();
+        assert leetCode220.containsNearbyAlmostDuplicate(new int[]{-3, 3, -6}, 2, 3);
+    }
+
+    @Test
+    public void testCase6() {
+        LeetCode220 leetCode220 = new LeetCode220();
+        boolean ans = leetCode220.containsNearbyAlmostDuplicate(new int[]{2, 0, -2, 2}, 2, 1);
+        assert !ans;
     }
 }
