b45b9cf75da1c10ce91cd756994a58b2e2c6eff8
zhengbinbin <zhengbinbin@58.com>
2022-03-06 11:38:53 +0800

    每日一题

--- src/com/geekbing/lcp/Lcp39.java
+++ src/com/geekbing/lcp/Lcp39.java
@@ -2,14 +2,35 @@ package com.geekbing.lcp;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * todo
- *
  * @author bing
  */
 public class Lcp39 {
     public int minimumSwitchingTimes(int[][] source, int[][] target) {
-        return 0;
+        Map<Integer, Integer> sourceMap = buildMap(source);
+        Map<Integer, Integer> targetMap = buildMap(target);
+        int ans = 0;
+        for (int key : sourceMap.keySet()) {
+            int sourceVal = sourceMap.get(key);
+            int targetVal = targetMap.getOrDefault(key, 0);
+            if (sourceVal > targetVal) {
+                ans += sourceVal - targetVal;
+            }
+        }
+        return ans;
+    }
+
+    private Map<Integer, Integer> buildMap(int[][] matrix) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int[] line : matrix) {
+            for (int num : line) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+        return map;
     }
 
     @Test
