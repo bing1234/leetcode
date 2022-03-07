e995f4b817d305a61f02b5ffbaecc79fefa3847c
zhengbinbin <zhengbinbin@58.com>
2022-03-07 13:27:59 +0800

    每日一题

---
+++ src/com/geekbing/middle/LeetCode29.java
@@ -0,0 +1,26 @@
+package com.geekbing.middle;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * todo
+ *
+ * @author bing
+ */
+public class LeetCode29 {
+    public int divide(int dividend, int divisor) {
+        return 0;
+    }
+
+    @Test
+    public void testCase1() {
+        LeetCode29 leetCode29 = new LeetCode29();
+        assert leetCode29.divide(10, 3) == 3;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode29 leetCode29 = new LeetCode29();
+        assert leetCode29.divide(7, -3) == -2;
+    }
+}
