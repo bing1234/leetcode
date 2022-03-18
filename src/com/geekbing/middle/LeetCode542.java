95962b8719b461b418d910b31dcd455a6dd11fe3
zhengbinbin <zhengbinbin@58.com>
2022-03-18 10:43:57 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode542.java
+++ src/com/geekbing/middle/LeetCode542.java
@@ -9,7 +9,34 @@ import java.util.Arrays;
  */
 public class LeetCode542 {
     public int[][] updateMatrix(int[][] mat) {
-        return new int[][]{};
+        int row = mat.length, col = mat[0].length;
+        int[][] dp = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                dp[i][j] = mat[i][j] == 0 ? 0 : Integer.MAX_VALUE / 2;
+            }
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i - 1 >= 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                }
+                if (j - 1 >= 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                }
+            }
+        }
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 0; j--) {
+                if (i + 1 <= row - 1) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                }
+                if (j + 1 <= col - 1) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                }
+            }
+        }
+        return dp;
     }
 
     @Test
