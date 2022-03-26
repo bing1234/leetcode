555538b13cef0a63026f072a90d4636b8ac23299
zhengbinbin <zhengbinbin@58.com>
2022-03-26 09:15:41 +0800

    每日一题

---
+++ src/com/geekbing/LeetCode457.java
@@ -0,0 +1,31 @@
+package com.geekbing;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bing
+ */
+public class LeetCode457 {
+    public boolean circularArrayLoop(int[] nums) {
+        int slow = 0, fast = 0;
+        while ()
+    }
+
+    @Test
+    public void testCase1() {
+        LeetCode457 leetCode457 = new LeetCode457();
+        assert leetCode457.circularArrayLoop(new int[]{2, -1, 1, 2, 2});
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode457 leetCode457 = new LeetCode457();
+        assert !leetCode457.circularArrayLoop(new int[]{-1, 2});
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode457 leetCode457 = new LeetCode457();
+        assert !leetCode457.circularArrayLoop(new int[]{-2, 1, -1, -2, -2});
+    }
+}
