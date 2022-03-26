2a58b3f78359ad9dc3a12f002698ca97e6f05a46
zhengbinbin <zhengbinbin@58.com>
2022-03-26 14:01:10 +0800

    每日一题

---
+++ src/com/geekbing/middle/LeetCode208.java
@@ -0,0 +1,75 @@
+package com.geekbing.middle;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bing
+ */
+public class LeetCode208 {
+    private static class Trie {
+        private final Node root;
+
+        public Trie() {
+            root = new Node(' ');
+        }
+
+        public void insert(String word) {
+            Node cur = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (cur.childs[c - 'a'] == null) {
+                    cur.childs[c - 'a'] = new Node(c);
+                }
+                cur = cur.childs[c - 'a'];
+            }
+            cur.isWord = true;
+        }
+
+        public boolean search(String word) {
+            Node cur = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (cur.childs[c - 'a'] == null) {
+                    return false;
+                }
+                cur = cur.childs[c - 'a'];
+            }
+            return cur.isWord;
+        }
+
+        public boolean startsWith(String prefix) {
+            Node cur = root;
+            for (int i = 0; i < prefix.length(); i++) {
+                char c = prefix.charAt(i);
+                if (cur.childs[c - 'a'] == null) {
+                    return false;
+                }
+                cur = cur.childs[c - 'a'];
+            }
+            return true;
+        }
+
+        private static class Node {
+            private final char c;
+            private final Node[] childs;
+            private boolean isWord;
+
+            public Node(char c) {
+                this.c = c;
+                this.childs = new Node[26];
+                this.isWord = false;
+            }
+        }
+    }
+
+    @Test
+    public void testCase1() {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        assert trie.search("apple");
+        assert !trie.search("app");
+        assert trie.startsWith("app");
+        trie.insert("app");
+        assert trie.search("app");
+    }
+}
