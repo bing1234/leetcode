8e146cbb1fd0f93ab617d9ee3e95db8de0f11589
zhengbinbin <zhengbinbin@58.com>
2022-03-26 20:17:36 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode386.java
+++ src/com/geekbing/middle/LeetCode386.java
@@ -10,9 +10,63 @@ import java.util.List;
  * @author bing
  */
 public class LeetCode386 {
-    public List<Integer> lexicalOrder(int n) {
+    private Node root;
 
-        return new ArrayList<>();
+    public List<Integer> lexicalOrder(int n) {
+        root = new Node(' ');
+        for (int i = 1; i <= n; i++) {
+            insert(String.valueOf(i));
+        }
+        return travelTrees(root);
+    }
+
+    private List<Integer> travelTrees(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        for (Node node : root.childs) {
+            ans.addAll(travelTree(node, ""));
+        }
+        return ans;
+    }
+
+    private List<Integer> travelTree(Node root, String cur) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        if (root.isEnd) {
+            cur += root.val;
+            ans.add(Integer.parseInt(cur));
+        }
+        for (Node node : root.childs) {
+            if (node != null) {
+                ans.addAll(travelTree(node, cur));
+            }
+        }
+        return ans;
+    }
+
+    private void insert(String num) {
+        Node cur = root;
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            if (cur.childs[c - '0'] == null) {
+                cur.childs[c - '0'] = new Node(c);
+            }
+            cur = cur.childs[c - '0'];
+        }
+        cur.isEnd = true;
+    }
+
+    private static class Node {
+        private char val;
+        private Node[] childs;
+        private boolean isEnd;
+
+        public Node(char val) {
+            this.val = val;
+            this.childs = new Node[10];
+            this.isEnd = false;
+        }
     }
 
     @Test
