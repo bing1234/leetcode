95f4dbbb9fce5b2f88ff8b5020351e7e9ded5f2d
zhengbinbin <zhengbinbin@58.com>
2022-03-26 11:28:34 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode1670.java
+++ src/com/geekbing/middle/LeetCode1670.java
@@ -7,56 +7,84 @@ import org.junit.jupiter.api.Test;
  */
 public class LeetCode1670 {
     private static class FrontMiddleBackQueue {
-        private Node head;
-        private Node middle;
-        private Node tail;
+        private final DoubleEndedLink firstDoubleEndedLink;
+        private final DoubleEndedLink secondDoubleEndedLink;
 
         public FrontMiddleBackQueue() {
-            head = new Node();
-            tail = new Node();
-            head.next = tail;
-            tail.prev = head;
+            firstDoubleEndedLink = new DoubleEndedLink();
+            secondDoubleEndedLink = new DoubleEndedLink();
         }
 
         public void pushFront(int val) {
-            Node cur = new Node(val);
-            cur.prev = head;
-            cur.next = head.next;
-            head.next.prev = cur;
-            head.next = cur;
+            firstDoubleEndedLink.pushFront(val);
+            adjust();
         }
 
         public void pushMiddle(int val) {
-            
+            if (firstDoubleEndedLink.size < secondDoubleEndedLink.size) {
+                firstDoubleEndedLink.pushBack(val);
+            } else {
+                secondDoubleEndedLink.pushFront(val);
+            }
         }
 
         public void pushBack(int val) {
-            Node cur = new Node(val);
-            cur.prev = tail.prev;
-            cur.next = tail;
-            tail.prev.next = cur;
-            tail.prev = cur;
+            secondDoubleEndedLink.pushBack(val);
+            adjust();
         }
 
         public int popFront() {
-            if (head.next == tail) {
-                return -1;
+            if (firstDoubleEndedLink.isNotEmpty()) {
+                int ans = firstDoubleEndedLink.popFront();
+                adjust();
+                return ans;
             }
-            Node cur = head.next;
-            cur.prev.next = cur.next;
-            cur.next.prev = cur.prev;
-            return cur.val;
+            if (secondDoubleEndedLink.isNotEmpty()) {
+                int ans = secondDoubleEndedLink.popFront();
+                adjust();
+                return ans;
+            }
+            return -1;
         }
 
         public int popMiddle() {
-            return 0;
+            if (firstDoubleEndedLink.isEmpty() && secondDoubleEndedLink.isEmpty()) {
+                return -1;
+            }
+            if (firstDoubleEndedLink.isEmpty()) {
+                return secondDoubleEndedLink.popFront();
+            }
+            if (secondDoubleEndedLink.isEmpty()) {
+                return firstDoubleEndedLink.popBack();
+            }
+            if (firstDoubleEndedLink.size < secondDoubleEndedLink.size) {
+                return secondDoubleEndedLink.popFront();
+            } else {
+                return firstDoubleEndedLink.popBack();
+            }
         }
 
         public int popBack() {
-            Node cur = tail.prev;
-            cur.prev.next = cur.next;
-            cur.next.prev = cur.prev;
-            return cur.val;
+            if (secondDoubleEndedLink.isNotEmpty()) {
+                int ans = secondDoubleEndedLink.popBack();
+                adjust();
+                return ans;
+            }
+            if (firstDoubleEndedLink.isNotEmpty()) {
+                int ans = firstDoubleEndedLink.popBack();
+                adjust();
+                return ans;
+            }
+            return -1;
+        }
+
+        private void adjust() {
+            if (firstDoubleEndedLink.size > secondDoubleEndedLink.size) {
+                secondDoubleEndedLink.pushFront(firstDoubleEndedLink.popBack());
+            }
+            if (secondDoubleEndedLink.size - firstDoubleEndedLink.size >= 2) {
+                firstDoubleEndedLink.pushBack(secondDoubleEndedLink.popFront());
+            }
         }
 
         private static class Node {
@@ -69,6 +97,68 @@ public class LeetCode1670 {
 
             public Node(int val) {
                 this.val = val;
+            }
+        }
+
+        private static class DoubleEndedLink {
+            private final Node head;
+            private final Node tail;
+            private int size;
+
+            public DoubleEndedLink() {
+                head = new Node();
+                tail = new Node();
+                head.next = tail;
+                tail.prev = head;
+                size = 0;
+            }
+
+            private void pushFront(int val) {
+                Node cur = new Node(val);
+                cur.prev = head;
+                cur.next = head.next;
+                head.next.prev = cur;
+                head.next = cur;
+                size++;
+            }
+
+            private void pushBack(int val) {
+                Node cur = new Node(val);
+                cur.prev = tail.prev;
+                cur.next = tail;
+                tail.prev.next = cur;
+                tail.prev = cur;
+                size++;
+            }
+
+            private int popFront() {
+                if (isEmpty()) {
+                    return -1;
+                }
+                Node cur = head.next;
+                cur.next.prev = cur.prev;
+                cur.prev.next = cur.next;
+                size--;
+                return cur.val;
+            }
+
+            private int popBack() {
+                if (isEmpty()) {
+                    return -1;
+                }
+                Node cur = tail.prev;
+                cur.next.prev = cur.prev;
+                cur.prev.next = cur.next;
+                size--;
+                return cur.val;
+            }
+
+            private boolean isEmpty() {
+                return size == 0;
+            }
+
+            private boolean isNotEmpty() {
+                return !isEmpty();
             }
         }
     }
