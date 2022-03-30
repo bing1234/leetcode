298fcbd9d7cdf25a1419e978d2747d34bde3a7d0
zhengbinbin <zhengbinbin@58.com>
2022-03-30 12:14:16 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode820.java
+++ src/com/geekbing/middle/LeetCode820.java
@@ -2,12 +2,71 @@ package com.geekbing.middle;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * @author bing
  */
 public class LeetCode820 {
     public int minimumLengthEncoding(String[] words) {
-        return 0;
+        Trie trie = new Trie();
+        Set<String> set = Arrays.stream(words).collect(Collectors.toSet());
+        for (String word : set) {
+            trie.insertWord(word);
+        }
+        int ans = 0;
+        for (String word : set) {
+            if (!trie.endWith(word)) {
+                ans += word.length() + 1;
+            }
+        }
+        return ans;
+    }
+
+    private static class Trie {
+        private final Node root;
+
+        public Trie() {
+            this.root = new Node();
+        }
+
+        private void insertWord(String word) {
+            Node cur = root;
+            for (int i = word.length() - 1; i >= 0; i--) {
+                char c = word.charAt(i);
+                if (cur.childs[c - 'a'] == null) {
+                    cur.childs[c - 'a'] = new Node();
+                }
+                cur = cur.childs[c - 'a'];
+            }
+        }
+
+        private boolean endWith(String word) {
+            Node cur = root;
+            for (int i = word.length() - 1; i >= 0; i--) {
+                char c = word.charAt(i);
+                if (cur.childs[c - 'a'] == null) {
+                    return false;
+                }
+                cur = cur.childs[c - 'a'];
+            }
+            for (Node child : cur.childs) {
+                if (child != null) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private static class Node {
+        private final Node[] childs;
+
+        public Node() {
+            this.childs = new Node[26];
+        }
     }
 
     @Test
@@ -20,5 +79,11 @@ public class LeetCode820 {
     public void testCase2() {
         LeetCode820 leetCode820 = new LeetCode820();
         assert leetCode820.minimumLengthEncoding(new String[]{"t"}) == 2;
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode820 leetCode820 = new LeetCode820();
+        assert leetCode820.minimumLengthEncoding(new String[]{"time", "time", "time", "time"}) == 5;
     }
 }
