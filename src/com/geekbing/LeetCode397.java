afe87298087fbb1b9325909dbd0f400578aba631
zhengbinbin <zhengbinbin@58.com>
2022-04-01 14:18:13 +0800

    每日一题

---
+++ src/com/geekbing/LeetCode397.java
@@ -0,0 +1,42 @@
+package com.geekbing;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bing
+ */
+public class LeetCode397 {
+    public int integerReplacement(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return 1 + integerReplacement(n / 2);
+        }
+        return 2 + Math.min(integerReplacement(n / 2), integerReplacement(n / 2 + 1));
+    }
+
+    @Test
+    public void testCase1() {
+        LeetCode397 leetCode397 = new LeetCode397();
+        assert leetCode397.integerReplacement(8) == 3;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode397 leetCode397 = new LeetCode397();
+        assert leetCode397.integerReplacement(7) == 4;
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode397 leetCode397 = new LeetCode397();
+        assert leetCode397.integerReplacement(4) == 2;
+    }
+
+    @Test
+    public void testCase4() {
+        LeetCode397 leetCode397 = new LeetCode397();
+        assert leetCode397.integerReplacement(1234) == 14;
+    }
+}
