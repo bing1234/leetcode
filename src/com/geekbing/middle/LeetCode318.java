0281594890ea2d69f8a01cf065a3d109d4374971
zhengbinbin <zhengbinbin@58.com>
2022-04-17 09:49:14 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode318.java
+++ src/com/geekbing/middle/LeetCode318.java
@@ -7,7 +7,23 @@ import org.junit.jupiter.api.Test;
  */
 public class LeetCode318 {
     public int maxProduct(String[] words) {
-        return 0;
+        int[] bits = new int[words.length];
+        // 将一个单词映射为一个整数
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                bits[i] |= 1 << (words[i].charAt(j) - 'a');
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if ((bits[i] & bits[j]) == 0) {
+                    ans = Math.max(ans, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return ans;
     }
 
     @Test
