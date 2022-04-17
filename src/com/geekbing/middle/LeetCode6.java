76315523439d41ac3cfd56a2b99febfae72001f6
zhengbinbin <zhengbinbin@58.com>
2022-04-17 10:04:45 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode6.java
+++ src/com/geekbing/middle/LeetCode6.java
@@ -2,12 +2,41 @@ package com.geekbing.middle;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * @author bing
  */
 public class LeetCode6 {
     public String convert(String s, int numRows) {
-        return "";
+        if (numRows == 1) {
+            return s;
+        }
+        String[] lines = new String[numRows];
+        Arrays.fill(lines, "");
+        int curLine = 0;
+        boolean asc = true;
+        for (int i = 0; i < s.length(); i++) {
+            lines[curLine] += s.charAt(i);
+            if (curLine == 0) {
+                curLine++;
+                asc = true;
+            } else if (curLine == numRows - 1) {
+                curLine--;
+                asc = false;
+            } else {
+                if (asc) {
+                    curLine++;
+                } else {
+                    curLine--;
+                }
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        for (String line : lines) {
+            ans.append(line);
+        }
+        return ans.toString();
     }
 
     @Test
