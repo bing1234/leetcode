3c33653819c4f4ddf2448e46bb3fe38504d93939
zhengbinbin <zhengbinbin@58.com>
2022-04-17 18:36:23 +0800

    每日一题

--- src/com/geekbing/LeetCode473.java
+++ src/com/geekbing/LeetCode473.java
@@ -2,11 +2,29 @@ package com.geekbing;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * @author bing
  */
 public class LeetCode473 {
     public boolean makesquare(int[] matchsticks) {
+        int sum = Arrays.stream(matchsticks).sum();
+        if (sum % 4 != 0) {
+            return false;
+        }
+        int len = sum / 4, cnt = 0;
+        for (int num : matchsticks) {
+            if (num > len) {
+                return false;
+            } else if (num == len) {
+                cnt++;
+            }
+        }
+        if (cnt == 4) {
+            return true;
+        }
+        
         return false;
     }
 
