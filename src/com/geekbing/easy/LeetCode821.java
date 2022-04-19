d8ba4066539f26bc6717c6d337dd235d9a5cea2e
zhengbinbin <zhengbinbin@58.com>
2022-04-19 12:48:29 +0800

    每日一题

--- src/com/geekbing/easy/LeetCode821.java
+++ src/com/geekbing/easy/LeetCode821.java
@@ -1,32 +1,50 @@
 package com.geekbing.easy;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * @author bing
+ */
 public class LeetCode821 {
-    public int[] shortestToChar(String S, char C) {
-        int[] ans = new int[S.length()];
-        List<Integer> idxs = new ArrayList<>();
-        for (int i = 0; i < S.length(); i++) {
-            if (S.charAt(i) == C) {
-                idxs.add(i);
+    public int[] shortestToChar(String s, char c) {
+        int len = s.length();
+        int[] ans = new int[len];
+        // 从左到右
+        int idx = -len;
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == c) {
+                idx = i;
             }
+            ans[i] = i - idx;
         }
-        for (int i = 0; i < S.length(); i++) {
-            int min = Integer.MAX_VALUE;
-            for (int idx : idxs) {
-                if (min > Math.abs(idx - i)) {
-                    min = Math.abs(idx - i);
-                }
+        // 从右到左
+        idx = 2 * len;
+        for (int i = len - 1; i >= 0; i--) {
+            if (s.charAt(i) == c) {
+                idx = i;
             }
-            ans[i] = min;
+            ans[i] = Math.min(ans[i], idx - i);
         }
         return ans;
     }
 
-    public static void main(String[] args) {
+    @Test
+    public void testCase1() {
         LeetCode821 leetCode821 = new LeetCode821();
-        System.out.println(Arrays.toString(leetCode821.shortestToChar("loveleetcode", 'e')));
+        int[] ans = leetCode821.shortestToChar("loveleetcode", 'e');
+        int[] expert = new int[]{3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0};
+        assert Arrays.equals(expert, ans);
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode821 leetCode821 = new LeetCode821();
+        int[] ans = leetCode821.shortestToChar("aaab", 'b');
+        int[] expert = new int[]{3, 2, 1, 0};
+        assert Arrays.equals(expert, ans);
     }
 }
