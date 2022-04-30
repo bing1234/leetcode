0870184e21684c1b19b559860a148b9c9a080b9a
zhengbinbin <zhengbinbin@58.com>
2022-04-30 09:47:41 +0800

    每日一题

---
+++ src/com/geekbing/LeetCode694.java
@@ -0,0 +1,86 @@
+package com.geekbing;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bing
+ */
+public class LeetCode694 {
+    public int numDistinctIslands(int[][] grid) {
+        UnionFind unionFind = new UnionFind(grid);
+        int row = grid.length, col = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                // 左右合并
+                if (j + 1 < col && grid[i][j] == 1 && grid[i][j + 1] == 1) {
+                    unionFind.union(i * col + j, i * col + j + 1);
+                }
+                // 上下合并
+                if (i + 1 < row && grid[i][j] == 1 && grid[i + 1][j] == 1) {
+                    unionFind.union(i * col + j, (i + 1) * col + j);
+                }
+            }
+        }
+
+
+
+    }
+
+    private static class UnionFind {
+        private final int[] parent;
+        private final int[] weight;
+
+        public UnionFind(int[][] grid) {
+            int row = grid.length, col = grid[0].length;
+            parent = new int[row * col];
+            weight = new int[row * col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (grid[i][j] == 1) {
+                        weight[i * col + j] = 1;
+                    }
+                }
+            }
+        }
+
+        public int find(int x) {
+            return parent[x] == x ? x : find(parent[x]);
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (weight[rootX] < weight[rootY]) {
+                    weight[rootY] += weight[rootX];
+                    parent[rootX] = rootY;
+                } else {
+                    weight[rootX] += weight[rootY];
+                    parent[rootY] = rootX;
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testCase1() {
+        LeetCode694 leetCode694 = new LeetCode694();
+        assert leetCode694.numDistinctIslands(new int[][]{
+                {1, 1, 0, 0, 0},
+                {1, 1, 0, 0, 0},
+                {0, 0, 0, 1, 1},
+                {0, 0, 0, 1, 1}
+        }) == 1;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode694 leetCode694 = new LeetCode694();
+        assert leetCode694.numDistinctIslands(new int[][]{
+                {1, 1, 0, 1, 1},
+                {1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1},
+                {1, 1, 0, 1, 1}
+        }) == 1;
+    }
+}
