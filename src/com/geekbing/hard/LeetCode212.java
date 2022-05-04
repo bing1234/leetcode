c2a3070ab4a046cc2e0cb8c3ed0c9c715512715b
zhengbinbin <zhengbinbin@58.com>
2022-05-04 11:43:00 +0800

    每日一题

--- src/com/geekbing/hard/LeetCode212.java
+++ src/com/geekbing/hard/LeetCode212.java
@@ -1,77 +1,130 @@
 package com.geekbing.hard;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
+/**
+ * @author bing
+ */
 public class LeetCode212 {
+    private final Set<String> ans = new HashSet<>();
+
     public List<String> findWords(char[][] board, String[] words) {
-        List<String> ans = new ArrayList<>();
+        Trie trie = new Trie();
         for (String word : words) {
-            if (canFind(board, word)) {
-                ans.add(word);
+            trie.insertWord(word);
+        }
+        int row = board.length, col = board[0].length;
+        boolean[][] visited = new boolean[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                dfs(board, visited, i, j, "", trie);
             }
         }
-        return ans;
+        return new ArrayList<>(ans);
     }
 
-    private boolean canFind(char[][] board, String word) {
-        boolean[][] visited = new boolean[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (dfs(board, word, visited, i, j, 0)) {
-                    return true;
-                }
-            }
+    private void dfs(char[][] board, boolean[][] visited, int i, int j, String str, Trie trie) {
+        // 边界检查
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+            return;
         }
-        return false;
-    }
-
-    private boolean dfs(char[][] board, String word, boolean[][] visited, int i, int j, int start) {
-        if (board[i][j] != word.charAt(start)) {
-            return false;
+        if (visited[i][j]) {
+            return;
         }
-        // 最后一个字母匹配
-        if (start == word.length() - 1) {
-            return true;
+        str += board[i][j];
+        if (!trie.searchPrefix(str)) {
+            return;
+        }
+        if (trie.searchWord(str)) {
+            ans.add(str);
         }
         visited[i][j] = true;
-        // 朝右
-        if (j + 1 < board[0].length &&
-                !visited[i][j + 1] &&
-                dfs(board, word, visited, i, j + 1, start + 1)) {
-            return true;
-        }
-        // 朝左
-        if (j - 1 >= 0 &&
-                !visited[i][j - 1] &&
-                dfs(board, word, visited, i, j - 1, start + 1)) {
-            return true;
-        }
-        // 朝下
-        if (i + 1 < board.length &&
-                !visited[i + 1][j] &&
-                dfs(board, word, visited, i + 1, j, start + 1)) {
-            return true;
-        }
         // 朝上
-        if (i - 1 >= 0 &&
-                !visited[i - 1][j] &&
-                dfs(board, word, visited, i - 1, j, start + 1)) {
-            return true;
-        }
+        dfs(board, visited, i - 1, j, str, trie);
+        // 朝下
+        dfs(board, visited, i + 1, j, str, trie);
+        // 朝左
+        dfs(board, visited, i, j - 1, str, trie);
+        // 朝右
+        dfs(board, visited, i, j + 1, str, trie);
         visited[i][j] = false;
-        return false;
     }
 
-    public static void main(String[] args) {
-        LeetCode212 leetCode212 = new LeetCode212();
+    private static class Trie {
+        private final Node root;
 
-        char[][] board = new char[4][4];
-        board[0] = new char[]{'o', 'a', 'a', 'n'};
-        board[1] = new char[]{'e', 't', 'a', 'e'};
-        board[2] = new char[]{'i', 'h', 'k', 'r'};
-        board[3] = new char[]{'i', 'f', 'l', 'v'};
-        String[] words = new String[]{"oath", "pea", "eat", "rain"};
-        System.out.println(leetCode212.findWords(board, words));
+        public Trie() {
+            root = new Node();
+        }
+
+        public void insertWord(String word) {
+            Node cur = root;
+            for (char c : word.toCharArray()) {
+                if (cur.childs[c - 'a'] == null) {
+                    cur.childs[c - 'a'] = new Node();
+                }
+                cur = cur.childs[c - 'a'];
+            }
+            cur.isWord = true;
+        }
+
+        public boolean searchWord(String word) {
+            Node cur = root;
+            for (char c : word.toCharArray()) {
+                if (cur.childs[c - 'a'] == null) {
+                    return false;
+                }
+                cur = cur.childs[c - 'a'];
+            }
+            return cur.isWord;
+        }
+
+        public boolean searchPrefix(String word) {
+            Node cur = root;
+            for (char c : word.toCharArray()) {
+                if (cur.childs[c - 'a'] == null) {
+                    return false;
+                }
+                cur = cur.childs[c - 'a'];
+            }
+            return true;
+        }
+    }
+
+    private static class Node {
+        private final Node[] childs;
+        private boolean isWord;
+
+        public Node() {
+            childs = new Node[26];
+            isWord = false;
+        }
+    }
+
+    @Test
+    public void testCase1() {
+        LeetCode212 leetCode212 = new LeetCode212();
+        List<String> ans = leetCode212.findWords(new char[][]{
+                {'o', 'a', 'a', 'n'},
+                {'e', 't', 'a', 'e'},
+                {'i', 'h', 'k', 'r'},
+                {'i', 'f', 'l', 'v'}
+        }, new String[]{"oath", "pea", "eat", "rain"});
+        List<String> expert = new ArrayList<>(Arrays.asList("eat", "oath"));
+        Collections.sort(ans);
+        assert expert.equals(ans);
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode212 leetCode212 = new LeetCode212();
+        List<String> ans = leetCode212.findWords(new char[][]{
+                {'a', 'b'},
+                {'c', 'd'}
+        }, new String[]{"abcb"});
+        Collections.sort(ans);
+        assert new ArrayList<>().equals(ans);
     }
 }
