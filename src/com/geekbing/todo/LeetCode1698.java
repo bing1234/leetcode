a0f836805351b61e07cc207be5bd7074ecdaaf9a
zhengbinbin <zhengbinbin@58.com>
2022-05-09 09:23:46 +0800

    每日一题

---
+++ src/com/geekbing/todo/LeetCode1698.java
@@ -0,0 +1,24 @@
+package com.geekbing.todo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bing
+ */
+public class LeetCode1698 {
+    public int countDistinct(String s) {
+        return 0;
+    }
+
+    @Test
+    public void testCase1() {
+        LeetCode1698 leetCode1698 = new LeetCode1698();
+        assert leetCode1698.countDistinct("aabbaba") == 1;
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode1698 leetCode1698 = new LeetCode1698();
+        assert leetCode1698.countDistinct("abcdefg") == 1;
+    }
+}
