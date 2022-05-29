f13f2c2ed20148865124fd0a0d38826ace385e16
zhengbinbin <zhengbinbin@58.com>
2022-05-29 21:19:45 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode468.java
+++ src/com/geekbing/middle/LeetCode468.java
@@ -1,15 +1,94 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author bing
  */
 public class LeetCode468 {
-    public String validIPAddress(String IP) {
-        return "";
+    public String validIPAddress(String queryIP) {
+        if (queryIP == null || queryIP.length() == 0 || queryIP.startsWith(".") || queryIP.endsWith(".") || queryIP.startsWith(":") || queryIP.endsWith(":")) {
+            return "Neither";
+        }
+        if (queryIP.contains(".")) {
+            return isIpV4(queryIP) ? "IPv4" : "Neither";
+        }
+        return isIpV6(queryIP) ? "IPv6" : "Neither";
     }
 
-    public static void main(String[] args) {
+    private boolean isIpV4(String ip) {
+        String[] items = ip.split("\\.");
+        if (items.length != 4) {
+            return false;
+        }
+        for (String item : items) {
+            if (item.length() < 1 || item.length() > 3 || (item.length() > 1 && item.charAt(0) == '0')) {
+                return false;
+            }
+            try {
+                int num = Integer.parseInt(item);
+                if (num < 0 || num > 255) {
+                    return false;
+                }
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private boolean isIpV6(String ip) {
+        String[] items = ip.split(":");
+        if (items.length != 8) {
+            return false;
+        }
+        for (String item : items) {
+            if (item.length() < 1 || item.length() > 4) {
+                return false;
+            }
+            for (char c : item.toCharArray()) {
+                if (!(Character.isDigit(c) || ('a' <= c && c <= 'f') || ('A' <= c && c <= 'F'))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void testCase1() {
         LeetCode468 leetCode468 = new LeetCode468();
-        System.out.println(leetCode468);
+        assert leetCode468.validIPAddress("172.16.254.1").equals("IPv4");
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode468 leetCode468 = new LeetCode468();
+        assert leetCode468.validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334").equals("IPv6");
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode468 leetCode468 = new LeetCode468();
+        assert leetCode468.validIPAddress("256.256.256.256").equals("Neither");
+    }
+
+    @Test
+    public void testCase4() {
+        LeetCode468 leetCode468 = new LeetCode468();
+        assert leetCode468.validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334:").equals("Neither");
+    }
+
+    @Test
+    public void testCase5() {
+        LeetCode468 leetCode468 = new LeetCode468();
+        assert leetCode468.validIPAddress("12..33.4").equals("Neither");
+    }
+
+    @Test
+    public void testCase6() {
+        LeetCode468 leetCode468 = new LeetCode468();
+        assert leetCode468.validIPAddress("20EE:FGb8:85a3:0:0:8A2E:0370:7334").equals("Neither");
     }
 }
--- src/com/geekbing/middle/LeetCode486.java
+++ src/com/geekbing/middle/LeetCode486.java
@@ -1,5 +1,10 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bing
+ */
 public class LeetCode486 {
     public boolean PredictTheWinner(int[] nums) {
         // dp[i,j]表示nums[i,j]先手获得的利润
@@ -15,9 +20,50 @@ public class LeetCode486 {
         return dp[0][nums.length - 1] > 0;
     }
 
-    public static void main(String[] args) {
+    public boolean PredictTheWinner2(int[] nums) {
+        int sum = arrSubSum(nums, 0, nums.length - 1);
+        int max = maxCanGet(nums, 0, nums.length - 1);
+        return max >= sum - max;
+    }
+
+    private int maxCanGet(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
+        }
+        if (right - left == 1) {
+            return Math.max(nums[left], nums[right]);
+        }
+        int sum = arrSubSum(nums, left, right);
+        // 如果选择最左边能够得到的最大值
+        int chooseLeft = sum - maxCanGet(nums, left + 1, right);
+        // 如果选择最右边能够得到的最大值
+        int chooseRight = sum - maxCanGet(nums, left, right - 1);
+        return Math.max(chooseLeft, chooseRight);
+    }
+
+    private int arrSubSum(int[] nums, int left, int right) {
+        int sum = 0;
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    @Test
+    public void testCase1() {
         LeetCode486 leetCode486 = new LeetCode486();
-        System.out.println(leetCode486.PredictTheWinner(new int[]{1, 5, 2}));
-        System.out.println(leetCode486.PredictTheWinner(new int[]{1, 5, 233, 7}));
+        assert !leetCode486.PredictTheWinner(new int[]{1, 5, 2});
+    }
+
+    @Test
+    public void testCase2() {
+        LeetCode486 leetCode486 = new LeetCode486();
+        assert leetCode486.PredictTheWinner(new int[]{1, 5, 233, 7});
+    }
+
+    @Test
+    public void testCase3() {
+        LeetCode486 leetCode486 = new LeetCode486();
+        assert leetCode486.PredictTheWinner(new int[]{1, 567, 1, 1, 99, 100});
     }
 }
