56aad8297716c8ba2fc5a5cd79491e12dcc2b6d8
zhengbinbin <zhengbinbin@58.com>
2022-07-21 09:29:00 +0800

    每日一题

--- src/com/geekbing/middle/LeetCode662.java
+++ src/com/geekbing/middle/LeetCode662.java
@@ -1,21 +1,120 @@
 package com.geekbing.middle;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
- * todo
  * @author bing
  */
 public class LeetCode662 {
     public int widthOfBinaryTree(TreeNode root) {
-        return 0;
+        // 层次遍历
+        List<List<Node>> levels = levelTravel(root);
+        int max = 0;
+        for (List<Node> level : levels) {
+            max = Math.max(max, level.get(level.size() - 1).idx - level.get(0).idx + 1);
+        }
+        return max;
     }
 
-    private static class TreeNode {
+    private List<List<Node>> levelTravel(TreeNode root) {
+        List<List<Node>> levels = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(root, 0));
+        while (!queue.isEmpty()) {
+            List<Node> level = new ArrayList<>();
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                Node cur = queue.poll();
+                if (cur == null) {
+                    continue;
+                }
+                level.add(cur);
+                if (cur.left != null) {
+                    Node left = new Node(cur.left, cur.idx * 2 + 1);
+                    queue.offer(left);
+                }
+                if (cur.right != null) {
+                    Node right = new Node(cur.right, cur.idx * 2 + 2);
+                    queue.offer(right);
+                }
+            }
+            levels.add(level);
+        }
+        return levels;
+    }
+
+
+    public class Node extends TreeNode {
+        int idx;
+
+        public Node(TreeNode node, int idx) {
+            this.left = node.left;
+            this.right = node.right;
+            this.val = node.val;
+            this.idx = idx;
+        }
+    }
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
+
+        TreeNode() {
+        }
 
         TreeNode(int val) {
             this.val = val;
         }
     }
+
+    @Test
+    public void testCase1() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(9);
+        int ans = widthOfBinaryTree(root);
+        assert ans == 4;
+    }
+
+    @Test
+    public void testCase2() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(3);
+        assert widthOfBinaryTree(root) == 2;
+    }
+
+    @Test
+    public void testCase3() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(5);
+        int ans = widthOfBinaryTree(root);
+        assert ans == 2;
+    }
+
+    @Test
+    public void testCase4() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(5);
+        root.left.left.left = new TreeNode(6);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(9);
+        root.right.right.right = new TreeNode(7);
+        int ans = widthOfBinaryTree(root);
+        assert ans == 8;
+    }
+
 }
